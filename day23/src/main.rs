0ca06908bfbc79dc63ab06731d7a484abf23395d
Matt Keeter <matt.j.keeter@gmail.com>
2018-12-23 15:52:21 -0500

    Finally got Part 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,144 +1,97 @@
 use std::io::{self, Read};
 use regex::Regex;
 use std::collections::{HashMap, HashSet, VecDeque};
-use std::cmp::{min, max};
+use std::cmp::min;
 
-////////////////////////////////////////////////////////////////////////////////
+// Bounds represent a region with the bounds
+//   x + y + z <= bounds[0]
+//  -x + y + z <= bounds[1]
+//   x - y + z <= bounds[2]
+//  -x - y + z <= bounds[3]
+//   x + y - z <= bounds[4]
+//  etc
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct Bounds([i64; 8]);
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-struct Region {
-    xmin: i64, xmax: i64,
-    ymin: i64, ymax: i64,
-    zmin: i64, zmax: i64,
-}
-
-impl Region {
-    fn could_contain(&self, p: &Point) -> bool {
-        !self.could_not_contain(p)
+impl Bounds {
+    fn from_pt(pt: &(i64, i64, i64), r: i64) -> Bounds {
+        let (x, y, z) = pt;
+        let mut out = [0; 8];
+        for i in 0..8 {
+            out[i] = r + x * Self::sign(i, 0)
+                       + y * Self::sign(i, 1)
+                       + z * Self::sign(i, 2);
+        }
+        Bounds(out)
     }
 
-    fn could_not_contain(&self, p: &Point) -> bool {
-        p.xmax() < self.xmin || p.xmin() > self.xmax ||
-        p.ymax() < self.ymin || p.ymin() > self.ymax ||
-        p.zmax() < self.zmin || p.zmin() > self.zmax
+    fn sign(i: usize, axis: usize) -> i64 {
+        if i & (1 << axis) != 0 {
+             1
+        } else {
+            -1
+        }
     }
 
-    fn split(&self) -> Vec<Region> {
-        let xmid = (self.xmin + self.xmax) / 2;
-        let ymid = (self.ymin + self.ymax) / 2;
-        let zmid = (self.zmin + self.zmax) / 2;
-        (0..8).map(|i|
-            Region { xmin: if (i & 1) != 0 { self.xmin } else { xmid + 1 },
-                     xmax: if (i & 1) != 0 {      xmid } else { self.xmax },
-                     ymin: if (i & 2) != 0 { self.ymin } else { ymid + 1 },
-                     ymax: if (i & 2) != 0 {      ymid } else { self.ymax },
-                     zmin: if (i & 4) != 0 { self.zmin } else { zmid + 1 },
-                     zmax: if (i & 4) != 0 {      zmid } else { self.zmax },
-        }).collect()
+    fn opposite(i: usize) -> usize {
+        (!i) & 0b111
     }
 
-    fn max_side(&self) -> i64 {
-        [(self.xmax - self.xmin),
-         (self.ymax - self.ymin),
-         (self.zmax - self.zmin)].iter().max().unwrap().clone()
-    }
-}
-
-#[derive(Debug)]
-struct Point {
-    x: i64,
-    y: i64,
-    z: i64,
-    r: i64,
-}
-
-impl Point {
-    fn xmin(&self) -> i64 { self.x - self.r }
-    fn xmax(&self) -> i64 { self.x + self.r }
-    fn ymin(&self) -> i64 { self.y - self.r }
-    fn ymax(&self) -> i64 { self.y + self.r }
-    fn zmin(&self) -> i64 { self.z - self.r }
-    fn zmax(&self) -> i64 { self.z + self.r }
-}
-
-struct Worker {
-    pts: Vec<Point>,
-    best_score: usize,
-    best_pos: Vec<(i64, i64, i64)>,
-}
-
-impl Worker {
-    fn new(pts: Vec<Point>, starting_score: usize) -> Worker {
-        Worker { pts: pts, best_score: starting_score, best_pos: vec![] }
+    fn contains(&self, pt: &(i64, i64, i64)) -> bool {
+        let (x, y, z) = pt;
+        (0..8).all(|i| x * Self::sign(i, 0)
+                     + y * Self::sign(i, 1)
+                     + z * Self::sign(i, 2) <= self.0[i])
     }
 
-    fn get_active(&self, r: &Region, active: &Vec<usize>) -> Vec<usize> {
-        active.iter()
-            .filter(|i| r.could_contain(&self.pts[**i]))
-            .cloned()
-            .collect()
+    fn is_empty(&self) -> bool {
+        (0..8).any(|i| self.0[i] < -self.0[Self::opposite(i)])
     }
 
-    fn pointwise(&mut self, r: &Region, active: &Vec<usize>) {
-        let mut my_best_score = 0;
-        for x in r.xmin..=r.xmax {
-            for y in r.ymin..=r.ymax {
-                for z in r.zmin..=r.zmax {
-                    let score = active.iter()
-                        .map(|i| &self.pts[*i])
-                        .filter(|p| (x - p.x).abs() +
-                                    (y - p.y).abs() +
-                                    (z - p.z).abs() <= p.r)
-                        .count();
-                    my_best_score = max(score, my_best_score);
-                    if score > self.best_score {
-                        println!("Found new best score {} at {} {} {}", score, x, y, z);
-                        self.best_score = score;
-                        self.best_pos.clear();
+    fn intersection(&self, other: &Bounds) -> Bounds {
+        let mut out = [0; 8];
+        for i in 0..8 {
+            out[i] = min(self.0[i], other.0[i]);
+        }
+        Bounds(out)
+    }
+
+    fn corners(&self) -> Vec<(i64, i64, i64)> {
+        let mut out = Vec::new();
+        for axis in [1, 2, 4].iter() {
+            for offset in [false, true].iter() {
+                let mut x = 0;
+                let mut y = 0;
+                let mut z = 0;
+
+                for i in 0..8 {
+                    if ((i & axis) != 0) == *offset {
+                        x += self.0[i] * Self::sign(i, 0);
+                        y += self.0[i] * Self::sign(i, 1);
+                        z += self.0[i] * Self::sign(i, 2);
                     }
-                    if score >= self.best_score {
-                        println!("Found matching best score {} at {} {} {}", score, x, y, z);
-                        self.best_pos.push((x, y, z));
+                }
+                let xs = if x % 4 == 0 { vec![x / 4] }
+                         else { vec![x / 4, (x + 3) / 4] };
+                let ys = if y % 4 == 0 { vec![y / 4] }
+                         else { vec![y / 4, (y + 3) / 4] };
+                let zs = if z % 4 == 0 { vec![z / 4] }
+                         else { vec![z / 4, (z + 3) / 4] };
+
+                for x in xs.iter() {
+                    for y in ys.iter() {
+                        for z in zs.iter() {
+                            out.push((*x, *y, *z));
+                        }
                     }
                 }
             }
         }
-    }
-
-    fn run(&mut self) {
-        let r = Region {
-            xmin: self.pts.iter().map(Point::xmin).min().unwrap(),
-            xmax: self.pts.iter().map(Point::xmax).max().unwrap(),
-            ymin: self.pts.iter().map(Point::ymin).min().unwrap(),
-            ymax: self.pts.iter().map(Point::ymax).max().unwrap(),
-            zmin: self.pts.iter().map(Point::zmin).min().unwrap(),
-            zmax: self.pts.iter().map(Point::zmax).max().unwrap(),
-        };
-
-        let active = (0..self.pts.len()).into_iter().collect();
-        self.recurse(&r, &active);
-    }
-
-    fn recurse(&mut self, r: &Region, active: &Vec<usize>) {
-        if active.len() < self.best_score {
-            return;
-        }
-
-        if r.max_side() < 10 {
-            self.pointwise(r, active);
-            return;
-        }
-
-        let mut rs: Vec<_> = r.split().into_iter()
-            .map(|r| (self.get_active(&r, active), r))
-            .collect();
-        rs.sort_by_key(|(a, _)| a.len());
-
-        for (a, r) in rs.iter().rev() {
-            self.recurse(r, a);
-        }
+        return out;
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
     let mut buffer = String::new();
@@ -151,31 +104,61 @@ fn main() {
         .map(|line| re.captures_iter(line)
                 .map(|i| str::parse::<i64>(&i[1]).unwrap())
                 .collect::<Vec<i64>>())
-        .map(|v| Point { x: v[0], y: v[1], z: v[2], r: v[3] })
-        .collect::<Vec<_>>();
+        .map(|v| ((v[0], v[1], v[2]), v[3]))
+        .collect::<Vec<((i64, i64, i64), i64)>>();
 
-    let p = pts.iter().max_by_key(|p| p.r).unwrap();
+    let ((x, y, z), r) = pts.iter().max_by_key(|pt| pt.1).unwrap();
     let n = pts.iter()
-        .filter(|q| (p.x - q.x).abs() +
-                    (p.y - q.y).abs() +
-                    (p.z - q.z).abs() <= p.r)
+        .filter(|(pt, _)| (pt.0 - x).abs() +
+                          (pt.1 - y).abs() +
+                          (pt.2 - z).abs() <= *r)
         .count();
     println!("Part 1: {}", n);
 
     ////////////////////////////////////////////////////////////////////////////
-    let starting_score = pts.iter()
-        .flat_map(|p| vec![(p.x + p.r, p.y, p.z), (p.x - p.r, p.y, p.z),
-                           (p.x, p.y + p.r, p.z), (p.x, p.y - p.r, p.z),
-                           (p.x, p.y, p.z + p.r), (p.x, p.y, p.z - p.r)]
-                           .into_iter())
-        .map(|(x, y, z)| pts.iter().filter(|q| (x - q.x).abs() +
-                                               (y - q.y).abs() +
-                                               (z - q.z).abs() <= q.r)
-                           .count())
-        .max().unwrap();
-    println!("Got lower bound {}", starting_score);
-    let mut w = Worker::new(pts, starting_score);
-    w.run();
+    let mut bounds = pts.iter()
+        .map(|(pt, r)| Bounds::from_pt(pt, *r))
+        .collect::<Vec<_>>();
 
-    // 129293600 is too high
+    let mut test_points = Vec::new();
+    for p in bounds.iter() {
+        for c in p.corners() {
+            test_points.push(c);
+        }
+        for q in bounds.iter() {
+            let r = p.intersection(&q);
+            if !r.is_empty() {
+                for c in r.corners() {
+                    test_points.push(c);
+                }
+            }
+        }
+    }
+    test_points.sort();
+    test_points.dedup();
+    println!("Got {} test points", test_points.len());
+
+    let mut best_score = 0;
+    let mut best_dist = std::i64::MAX;
+    for (x, y, z) in test_points.iter() {
+        let n = pts.iter()
+            .filter(|(pt, r)| (pt.0 - x).abs() +
+                              (pt.1 - y).abs() +
+                              (pt.2 - z).abs() <= *r)
+            .count();
+
+
+        let dist = x + y + z;
+        if n > best_score {
+            best_score = n;
+            best_dist = dist;
+        }
+
+        if n == best_score {
+            if dist <= best_dist {
+                best_dist = dist;
+                println!("{}, ({}, {}, {}), {}", n, x, y, z, best_dist);
+            }
+        }
+    }
 }
