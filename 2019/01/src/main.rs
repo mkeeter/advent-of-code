5f4adb217a65bb08d84ebae04ed8367f3f74a85f
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-01 10:35:00 -0500

    2019, day 1

---
+++ 2019/01/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::str::FromStr;
+use std::iter::successors;
+
+fn fuel(mass: &i32) -> Option<i32> {
+    if *mass >= 6 {
+        Some(*mass / 3 - 2)
+    } else {
+        None
+    }
+}
+
+fn total_fuel(mass: &i32) -> i32 {
+    successors(Some(*mass), fuel).skip(1).sum()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let f = File::open("input").expect("file not found");
+    let f = BufReader::new(f);
+
+    let masses = f.lines()
+        .map(|line| i32::from_str(&line.unwrap()))
+        .map(|res| res.expect("Could not parse int"))
+        .collect::<Vec<i32>>();
+
+    println!("Part 1: {}", masses.iter().filter_map(fuel).sum::<i32>());
+    println!("Part 2: {}", masses.iter().map(total_fuel).sum::<i32>());
+
+    Ok(())
+}
