ec73be64fff16c96f00dcbecb5148584c9496fd3
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-05 08:38:58 -0500

    Part 2 working

--- 2019/05/src/main.rs
+++ 2019/05/src/main.rs
@@ -1,18 +1,25 @@
 use std::io::Read;
 use std::str::FromStr;
 
-const OP_ADD: i32 = 1;
-const OP_SUB: i32 = 2;
-const OP_INPUT: i32 = 3;
+const OP_ADD:    i32 = 1;
+const OP_SUB:    i32 = 2;
+const OP_INPUT:  i32 = 3;
 const OP_OUTPUT: i32 = 4;
-const OP_BREAK: i32 = 99;
+const OP_JIT:    i32 = 5;
+const OP_JIF:    i32 = 6;
+const OP_LT:     i32 = 7;
+const OP_EQ:     i32 = 8;
+const OP_BREAK:  i32 = 99;
+
+const MODE_POSITION:  i32 = 0;
+const MODE_IMMEDIATE: i32 = 1;
 
 fn param(mem: &Vec<i32>, ip: usize, index: u32) -> i32 {
     let m = (mem[ip] / 10_i32.pow(index + 2)) % 10;
     let arg = 1 + ip + index as usize;
     match m {
-        0 => mem[mem[arg] as usize],
-        1 => mem[arg],
+        MODE_POSITION  => mem[mem[arg] as usize],
+        MODE_IMMEDIATE => mem[arg],
         _ => panic!(),
     }
 }
@@ -38,13 +45,43 @@ fn run(mut mem: Vec<i32>, input: i32) -> Vec<i32> {
                 ip += 4;
             }
             OP_INPUT => {
-                let out = mem[ip + 1];
-                mem[out as usize] = input;
+                let out = mem[ip + 1] as usize;
+                mem[out] = input;
                 ip += 2;
             }
             OP_OUTPUT => {
                 output.push(param(&mem, ip, 0));
                 ip += 2;
+            }
+            OP_JIT => {
+                let p = param(&mem, ip, 0);
+                if p != 0 {
+                    ip = param(&mem, ip, 1) as usize;
+                } else {
+                    ip += 3;
+                }
+            }
+            OP_JIF => {
+                let p = param(&mem, ip, 0);
+                if p == 0 {
+                    ip = param(&mem, ip, 1) as usize;
+                } else {
+                    ip += 3;
+                }
+            }
+            OP_LT => {
+                let lhs = param(&mem, ip, 0);
+                let rhs = param(&mem, ip, 1);
+                let out = mem[ip + 3] as usize;
+                mem[out] = if lhs < rhs { 1 } else { 0 };
+                ip += 4;
+            }
+            OP_EQ => {
+                let lhs = param(&mem, ip, 0);
+                let rhs = param(&mem, ip, 1);
+                let out = mem[ip + 3] as usize;
+                mem[out] = if lhs == rhs { 1 } else { 0 };
+                ip += 4;
             }
             OP_BREAK => break,
             _ => panic!("Invalid opcode {}", opcode),
@@ -60,6 +97,14 @@ fn main() {
         .map(|i| i32::from_str(i))
         .map(|r| r.expect("Could not parse int"))
         .collect::<Vec<_>>();
-    let out = run(mem, 1);
-    println!("{:?}", out);
+
+    let out = run(mem.clone(), 1);
+    for o in out[0..out.len() - 1].iter() {
+        assert!(*o == 0);
+    }
+    println!("Part 1: {}", out[out.len() - 1]);
+
+    let out = run(mem.clone(), 5);
+    assert!(out.len() == 1);
+    println!("Part 2: {:?}", out[0]);
 }
