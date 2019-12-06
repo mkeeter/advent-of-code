553ef992a763b2e3e80a22b253930046ed93ec9c
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-06 08:31:33 -0500

    Switch to usize keys for planets

--- 2019/06/src/main.rs
+++ 2019/06/src/main.rs
@@ -2,39 +2,79 @@ use std::collections::{HashMap, HashSet};
 use std::io::BufRead;
 
 struct Orbits {
-    parents: HashMap<String, String>,
-    distances: HashMap<String, usize>,
+    names: HashMap<String, usize>,
+    parents: Vec<Option<usize>>,
+    distances: Vec<Option<usize>>,
 }
 
 impl Orbits {
-    fn distance(&mut self, s: &str) -> usize {
-        if let Some(d) = self.distances.get(s) {
-            *d
+    fn index(&mut self, s: &str) -> usize {
+        if let Some(i) = self.names.get(s) {
+            *i
         } else {
-            let parent = self.parents.get(s).unwrap().to_owned();
-            let d = self.distance(&parent) + 1;
-            self.distances.insert(s.to_string(), d);
+            let i = self.names.len();
+            self.names.insert(s.to_owned(), i);
+            self.parents.push(None);
+            self.distances.push(None);
+            i
+        }
+    }
+
+    fn new(parents: &HashMap<String, String>) -> Orbits {
+        let mut out = Orbits {
+            names: HashMap::new(),
+            parents: Vec::new(),
+            distances: Vec::new(),
+        };
+        for (k,v) in parents.iter() {
+            let k = out.index(k);
+            let v = out.index(v);
+            out.parents[k] = Some(v);
+        }
+        let com = out.index("COM");
+        out.distances[com] = Some(0);
+        out
+    }
+
+    fn _distance(&mut self, s: usize) -> usize {
+        if let Some(d) = self.distances[s] {
+            d
+        } else {
+            let parent = self.parents[s].unwrap().to_owned();
+            let d = self._distance(parent) + 1;
+            self.distances[s] = Some(d);
             d
         }
     }
 
     fn checksum(&mut self) -> usize {
         let mut out = 0;
-        let keys = self.parents.keys().cloned().collect::<Vec<_>>();
-        for p in keys {
-            out += self.distance(&p);
+        for i in 0..self.names.len() {
+            out += self._distance(i);
         }
         out
     }
 
-    fn parents_of(&self, s: &str) -> HashSet<String> {
+    fn _parents_of(&self, mut s: usize) -> HashSet<usize> {
         let mut out = HashSet::new();
-        let mut s = s;
-        while let Some(p) = self.parents.get(s) {
-            out.insert(p.to_owned());
+        while let Some(p) = self.parents[s] {
+            out.insert(p);
             s = p;
         }
         out
+    }
+
+    fn transfer(&self, a: &str, b: &str) -> usize {
+        let a = *self.names.get(a).unwrap();
+        let b = *self.names.get(b).unwrap();
+        let c = *self._parents_of(a)
+            .intersection(&self._parents_of(b))
+            .max_by_key(|o| self.distances[**o].unwrap())
+            .unwrap();
+
+        self.distances[a].unwrap() +
+        self.distances[b].unwrap() -
+        2*self.distances[c].unwrap() - 2
     }
 }
 
@@ -42,22 +82,13 @@ fn main() {
     let parents = std::io::stdin().lock().lines()
         .map(|line| line.unwrap())
         .map(|line| {
-            println!("{}", line);
             let mut itr = line.split(')');
             let orbitee = itr.next().unwrap().to_owned();
             let orbiter = itr.next().unwrap().to_owned();
             (orbiter, orbitee) })
         .collect::<HashMap<_,_>>();
 
-    let mut orbits = Orbits { parents: parents, distances: HashMap::new() };
-    orbits.distances.insert("COM".to_string(), 0);
+    let mut orbits = Orbits::new(&parents);
     println!("Part 1: {}", orbits.checksum());
-
-    let common = orbits.parents_of("YOU")
-        .intersection(&orbits.parents_of("SAN"))
-        .map(|o| {println!("{:?}", o); o})
-        .max_by_key(|o| orbits.distances.get(*o).unwrap())
-        .unwrap()
-        .to_owned();
-    println!("Part 2: {}", orbits.distance("YOU") + orbits.distance("SAN") - 2*orbits.distance(&common) - 2);
+    println!("Part 2: {}", orbits.transfer("YOU", "SAN"));
 }
