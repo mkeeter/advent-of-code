d4e5acf78c33cacb3bb2d674194edd951a705b8d
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-06 21:23:59 -0500

    2015, day 2

---
+++ 2015/02/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::BufRead;
+use std::str::FromStr;
+
+fn main() {
+    let sizes = std::io::stdin().lock().lines()
+        .map(|line| {
+            let mut v = line.unwrap()
+                 .split('x')
+                 .map(|s| usize::from_str(s).unwrap())
+                 .collect::<Vec<_>>();
+            v.sort();
+            (v[0], v[1], v[2]) })
+        .collect::<Vec<_>>();
+
+    let paper = sizes.iter()
+        .map(|(a, b, c)| 2*(a*b + b*c + a*c) + a*b)
+        .sum::<usize>();
+    println!("Part 1: {}", paper);
+
+    let ribbon = sizes.iter()
+        .map(|(a, b, c)| 2*(a + b) + a*b*c)
+        .sum::<usize>();
+    println!("Part 2: {}", ribbon);
+}
