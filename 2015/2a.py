98cee1acf92bccc88a1fb321d4bb50b2c636c0cc
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-06 21:24:20 -0500

    Remove old Python implementation

--- 2015/2a.py
+++
@@ -1,8 +0,0 @@
-def surf(w,h,l):
-    a = l*w
-    b = w*h
-    c = h*l
-    return 2*(a + b + c) + min(a, b, c)
-
-print sum([surf(*(map(int, line.split('x')))) for line in
-           open('2a.dat').readlines()])
