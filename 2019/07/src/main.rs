af6d0b7a24ab258c1b7d0cc9ffe927e8f655395b
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-07 09:48:42 -0500

    Rewrote part 1 to run in parallel

--- 2019/07/src/main.rs
+++ 2019/07/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::Read;
 use std::str::FromStr;
+use std::collections::VecDeque;
 
 const OP_ADD:    i64 = 1;
 const OP_MUL:    i64 = 2;
@@ -14,82 +15,103 @@ const OP_BREAK:  i64 = 99;
 const MODE_POSITION:  i64 = 0;
 const MODE_IMMEDIATE: i64 = 1;
 
-fn param(mem: &Vec<i64>, ip: usize, index: u32) -> i64 {
-    let m = (mem[ip] / 10_i64.pow(index + 1)) % 10;
-    let arg = ip + index as usize;
-    match m {
-        MODE_POSITION  => mem[mem[arg] as usize],
-        MODE_IMMEDIATE => mem[arg],
-        _ => panic!(),
-    }
+struct Vm {
+    mem: Vec<i64>,
+    ip: usize,
 }
 
-fn run<'a, I>(mut mem: Vec<i64>, mut input: I) -> Vec<i64>
-    where I: Iterator<Item=&'a i64>
-{
-    let mut ip = 0;
-    let mut output = Vec::new();
-    loop {
-        let opcode = mem[ip] % 100;
+#[derive(Debug, PartialEq)]
+enum VmResult {
+    Okay,
+    UsedInput,
+    NeedsInput,
+    Output(i64),
+    Done,
+}
+
+impl Vm {
+    fn param(&self, index: u32) -> i64 {
+        let m = (self.mem[self.ip] / 10_i64.pow(index + 1)) % 10;
+        let arg = self.ip + index as usize;
+        match m {
+            MODE_POSITION  => self.mem[self.mem[arg] as usize],
+            MODE_IMMEDIATE => self.mem[arg],
+            _ => panic!(),
+        }
+    }
+
+    fn step(&mut self, input: Option<&i64>) -> VmResult {
+        let opcode = self.mem[self.ip] % 100;
         match opcode {
             OP_ADD => {
-                let lhs = param(&mem, ip, 1);
-                let rhs = param(&mem, ip, 2);
-                let out = mem[ip + 3] as usize;
-                mem[out] = lhs + rhs;
-                ip += 4;
+                let lhs = self.param(1);
+                let rhs = self.param(2);
+                let out = self.mem[self.ip + 3] as usize;
+                self.mem[out] = lhs + rhs;
+                self.ip += 4;
+                VmResult::Okay
             }
             OP_MUL => {
-                let lhs = param(&mem, ip, 1);
-                let rhs = param(&mem, ip, 2);
-                let out = mem[ip + 3] as usize;
-                mem[out] = lhs * rhs;
-                ip += 4;
+                let lhs = self.param(1);
+                let rhs = self.param(2);
+                let out = self.mem[self.ip + 3] as usize;
+                self.mem[out] = lhs * rhs;
+                self.ip += 4;
+                VmResult::Okay
             }
             OP_INPUT => {
-                let out = mem[ip + 1] as usize;
-                mem[out] = *input.next().unwrap();
-                ip += 2;
+                if let Some(input) = input {
+                    let out = self.mem[self.ip + 1] as usize;
+                    self.mem[out] = *input;
+                    self.ip += 2;
+                    VmResult::UsedInput
+                } else {
+                    VmResult::NeedsInput
+                }
             }
             OP_OUTPUT => {
-                output.push(param(&mem, ip, 1));
-                ip += 2;
+                let out = self.param(1);
+                self.ip += 2;
+                VmResult::Output(out)
             }
             OP_JIT => {
-                let p = param(&mem, ip, 1);
+                let p = self.param(1);
                 if p != 0 {
-                    ip = param(&mem, ip, 2) as usize;
+                    self.ip = self.param(2) as usize;
                 } else {
-                    ip += 3;
+                    self.ip += 3;
                 }
+                VmResult::Okay
             }
             OP_JIF => {
-                let p = param(&mem, ip, 1);
+                let p = self.param(1);
                 if p == 0 {
-                    ip = param(&mem, ip, 2) as usize;
+                    self.ip = self.param(2) as usize;
                 } else {
-                    ip += 3;
+                    self.ip += 3;
                 }
+                VmResult::Okay
             }
             OP_LT => {
-                let lhs = param(&mem, ip, 1);
-                let rhs = param(&mem, ip, 2);
-                let out = mem[ip + 3] as usize;
-                mem[out] = if lhs < rhs { 1 } else { 0 };
-                ip += 4;
+                let lhs = self.param(1);
+                let rhs = self.param(2);
+                let out = self.mem[self.ip + 3] as usize;
+                self.mem[out] = if lhs < rhs { 1 } else { 0 };
+                self.ip += 4;
+                VmResult::Okay
             }
             OP_EQ => {
-                let lhs = param(&mem, ip, 1);
-                let rhs = param(&mem, ip, 2);
-                let out = mem[ip + 3] as usize;
-                mem[out] = if lhs == rhs { 1 } else { 0 };
-                ip += 4;
+                let lhs = self.param(1);
+                let rhs = self.param(2);
+                let out = self.mem[self.ip + 3] as usize;
+                self.mem[out] = if lhs == rhs { 1 } else { 0 };
+                self.ip += 4;
+                VmResult::Okay
             }
-            OP_BREAK => break,
+            OP_BREAK => VmResult::Done,
             _ => panic!("Invalid opcode {}", opcode),
         }
     }
-    output
 }
 
 fn main() {
@@ -104,13 +126,38 @@ fn main() {
     let mut phases = [0,1,2,3,4];
     let mut best = 0;
     permutohedron::heap_recursive(&mut phases, |ps| {
-        let mut input = 0;
-        for i in 0..5 {
-            input = run(mem.clone(), [ps[i], input].iter())[0];
-        }
-        if input > best {
-            println!("{} {:?}", input, ps);
-            best = input;
+        let mut vms = (0..5).into_iter()
+            .map(|_| Vm { mem: mem.clone(), ip: 0 })
+            .collect::<Vec<_>>();
+        let mut queues = (0..5).into_iter()
+            .map(|i| {
+                let mut q = VecDeque::new();
+                q.push_front(ps[i]);
+                q
+            })
+            .collect::<Vec<_>>();
+        queues[0].push_front(0);
+        loop {
+            let mut all_done = true;
+            for i in 0..5 {
+                let result = vms[i].step(queues[i].back());
+                all_done &= result == VmResult::Done;
+                match result {
+                    VmResult::UsedInput => { queues[i].pop_back(); () }
+                    VmResult::Output(o) => if i == 4 {
+                        if o > best {
+                            best = o;
+                        }
+                    } else {
+                        queues[i + 1].push_front(o);
+                    },
+                    _ => (),
+                }
+            }
+            if all_done {
+                break;
+            }
         }
     });
+    println!("Part 1: {}", best);
 }
