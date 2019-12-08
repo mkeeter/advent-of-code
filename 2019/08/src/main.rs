d5e176d30cd45354c4261d09f1c2a20a71981c31
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-08 10:48:01 -0500

    Look Ma, no mut!

--- 2019/08/src/main.rs
+++ 2019/08/src/main.rs
@@ -13,22 +13,25 @@ fn main() {
     let layer = data.chunks(width * height)
         .min_by_key(|layer| layer.iter().filter(|i| **i == 0).count())
         .unwrap();
-
     let num_ones = layer.iter().filter(|i| **i == 1).count();
     let num_twos = layer.iter().filter(|i| **i == 2).count();
     println!("Part 1: {}", num_ones * num_twos);
 
     println!("Part 2:");
-    let mut combined = vec![2; width * height];
-    for layer in data.chunks(width * height) {
-        combined.iter_mut()
-            .zip(layer.iter())
-            .filter(|(p, _)| **p == 2)
-            .for_each(|(p, q)|  *p = *q);
-    }
+    let combined = data
+        .chunks(width * height)
+        .fold(vec![2; width * height],
+            |combined, layer| {
+                combined.iter()
+                    .zip(layer.iter())
+                    .map(|(p, q)| if *p == 2 { *q } else { *p })
+                    .collect()
+                });
+
     let image = combined.iter()
         .map(|i| if *i == 1 { 'X' } else { ' ' })
         .collect::<Vec<_>>();
+
     for row in image.chunks(width) {
         println!("{}", row.iter().collect::<String>());
     }
