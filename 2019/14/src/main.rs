8605e434102ff25d48ec2ab2c16f5db684bcd8c5
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-14 11:30:13 -0500

    Lots of cleaning up

--- 2019/14/src/main.rs
+++ 2019/14/src/main.rs
@@ -1,14 +1,11 @@
 use std::collections::HashMap;
 use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufWriter, Write};
-use std::process::Command;
+use std::fmt::Write as FmtWrite;
+use std::io::{BufRead, Write};
+use std::process::{Command, Stdio};
 use std::str::FromStr;
 
-#[derive(Debug)]
-struct Mol(i32, String);
-
-fn parse(s: &str) -> (Vec<Mol>, Mol)
+fn parse(s: &str) -> Vec<(i32, String)>
 {
     let words = s.replace(|c| !char::is_alphanumeric(c), " ")
         .split(' ')
@@ -17,11 +14,13 @@ fn parse(s: &str) -> (Vec<Mol>, Mol)
         .collect::<Vec<String>>();
 
     let mut mols = words.chunks(2)
-        .map(|c| Mol(i32::from_str(&c[0]).unwrap(), c[1].clone()))
-        .collect::<Vec<Mol>>();
+        .map(|c| (-i32::from_str(&c[0]).unwrap(), c[1].clone()))
+        .collect::<Vec<(i32, String)>>();
 
-    let out = mols.remove(mols.len() - 1);
-    (mols, out)
+    // The output is the only positive value
+    let last = mols.len() - 1;
+    mols[last].0 *= -1;
+    mols
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -30,95 +29,92 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<_>>();
 
     let mut sums : HashMap<&str, Vec<(usize, i32)>> = HashMap::new();
-    for (i, (inputs, output)) in eqns.iter().enumerate() {
+    for (i, inputs) in eqns.iter().enumerate() {
         for mol in inputs {
-            sums.entry(&mol.1).or_insert(Vec::new()).push((i, -mol.0))
+            sums.entry(&mol.1).or_insert(Vec::new()).push((i, mol.0))
         }
-        sums.entry(&output.1).or_insert(Vec::new()).push((i, output.0))
+    }
+
+    // Build up a string containing all of our reaction constraints
+    let mut constraints = String::new();
+    writeln!(&mut constraints, "Subject to")?;
+    for (k, vs) in sums.iter() {
+        write!(&mut constraints, "  {}: ", k)?;
+        for v in vs.iter() {
+            if v.1 < 0 {
+                write!(&mut constraints, "-")?;
+            } else {
+                write!(&mut constraints, "+")?;
+            }
+            write!(&mut constraints, " {} eqn{} ", v.1.abs(), v.0)?;
+        }
+
+        if k == &"ORE" {
+            write!(&mut constraints, " + ore_in ")?
+        } else if k == &"FUEL" {
+            write!(&mut constraints, " - fuel_out ")?
+        }
+        writeln!(&mut constraints, ">= 0")?;
+    }
+
+    // Build up a string which marks that all variables are integers
+    let mut integers = String::new();
+    writeln!(&mut integers, "Integer
+  ore_in
+  fuel_out")?;
+    for i in 0..eqns.len() {
+        writeln!(&mut integers, "  eqn{}", i)?;
     }
 
     // Part 1
-    let tmp = File::create("part1.lp")?;
-    let mut out = BufWriter::new(&tmp);
-    writeln!(out, "Minimize in: ore_in
-
-Subject to")?;
-    for (k, vs) in sums.iter() {
-        write!(out, "  {}: ", k)?;
-        for v in vs.iter() {
-            if v.1 < 0 {
-                write!(out, "- {} eqn{} ", v.1.abs(), v.0)?;
-            } else {
-                write!(out, "+ {} eqn{} ", v.1, v.0)?;
-            }
-        }
-        if k == &"ORE" {
-            write!(out, " + ore_in ")?
-        }
-        if k == &"FUEL" {
-            writeln!(out, ">= 1")?;
-        } else {
-            writeln!(out, ">= 0")?;
-        }
-    }
-    writeln!(out, "  FUEL >= 1
-
-Integer")?;
-    for (i, _) in eqns.iter().enumerate() {
-        writeln!(out, "  eqn{}", i)?;
-    }
-    writeln!(out, "\nend")?;
-
-    Command::new("glpsol")
+    let mut subprocess = Command::new("glpsol")
             .arg("--lp")
-            .arg("part1.lp")
+            .arg("/dev/stdin")
             .arg("--output")
-            .arg("sol1.out")
+            .arg("/dev/stdout")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn()
-            .expect("failed to execute process");
+            .expect("Failed to spawn solver");
+    let pipe = subprocess.stdin.as_mut().expect("Failed to open stdin");
+    writeln!(pipe, "Minimize in: ore_in\n")?;
+    writeln!(pipe, "{}", constraints)?;
+    writeln!(pipe, "  fuel_out >= 1\n")?;
+    writeln!(pipe, "{}\n", integers)?;
+    writeln!(pipe, "end")?;
+
+    let output = subprocess.wait_with_output()?;
+    for line in String::from_utf8_lossy(&output.stdout).lines() {
+        if line.starts_with("Objective:") {
+            println!("Part 1: {}", line.replace(|c| !char::is_numeric(c), ""));
+        }
+    }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-    // Part 1
-    let tmp = File::create("part2.lp")?;
-    let mut out = BufWriter::new(&tmp);
-    writeln!(out, "Maximize out: fuel_out
-
-Subject to")?;
-    for (k, vs) in sums.iter() {
-        write!(out, "  {}: ", k)?;
-        for v in vs.iter() {
-            if v.1 < 0 {
-                write!(out, "- {} eqn{} ", v.1.abs(), v.0)?;
-            } else {
-                write!(out, "+ {} eqn{} ", v.1, v.0)?;
-            }
-        }
-        if k == &"ORE" {
-            write!(out, "+ ore_in ")?
-        }
-        if k == &"FUEL" {
-            writeln!(out, "- fuel_out >= 0")?;
-        } else {
-            writeln!(out, ">= 0")?;
-        }
-    }
-    writeln!(out, "  ore_in = 1000000000000
-
-Integer")?;
-    for (i, _) in eqns.iter().enumerate() {
-        writeln!(out, "  eqn{}", i)?;
-    }
-    writeln!(out, "  fuel_out
-  ore_in
-\nend")?;
-
-    Command::new("glpsol")
+    // Part 2
+    let mut subprocess = Command::new("glpsol")
             .arg("--lp")
-            .arg("part2.lp")
+            .arg("/dev/stdin")
             .arg("--output")
-            .arg("sol2.out")
+            .arg("/dev/stdout")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
             .spawn()
-            .expect("failed to execute process");
+            .expect("Failed to spawn solver");
+    let pipe = subprocess.stdin.as_mut().expect("Failed to open stdin");
+    writeln!(pipe, "Maximize out: fuel_out\n")?;
+    writeln!(pipe, "{}", constraints)?;
+    writeln!(pipe, "  ore_in = 1000000000000\n")?;
+    writeln!(pipe, "{}\n", integers)?;
+    writeln!(pipe, "end")?;
+
+    let output = subprocess.wait_with_output()?;
+    for line in String::from_utf8_lossy(&output.stdout).lines() {
+        if line.starts_with("Objective:") {
+            println!("Part 2: {}", line.replace(|c| !char::is_numeric(c), ""));
+        }
+    }
+
     Ok(())
 }
