41eca292e08c4687c745e875d441d4fec6e46c18
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-20 08:38:19 -0500

    2019, day 20, part 1

---
+++ 2019/20/src/main.rs
@@ -0,0 +1,69 @@
+use std::io::BufRead;
+use std::collections::{HashMap, HashSet, VecDeque};
+
+fn main() {
+    let mut tiles = HashMap::new();
+    for (y, line) in std::io::stdin().lock().lines().enumerate() {
+        for (x, c) in line.unwrap().chars().enumerate() {
+            tiles.insert((x as i32 + 1, y as i32 + 1), c);
+        }
+    }
+
+    let get = |x: i32, y: i32| { *tiles.get(&(x, y)).unwrap_or(&'#') };
+
+    let mut portals = tiles.iter()
+        .filter(|(_k, v)| **v == '.')
+        .flat_map(|(k, _v)| [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                  .iter()
+                  .map(move |d| (*k, *d)))
+        .filter_map(|((x, y), (dx, dy))| {
+            let a = get(x + 1 * dx, y + 1 * dy);
+            let b = get(x + 2 * dx, y + 2 * dy);
+            if char::is_uppercase(a) && char::is_uppercase(b) {
+                let key = if dx < 0 || dy < 0 {
+                    [b, a]
+                } else {
+                    [a, b]
+                };
+                Some((key, (x, y), (x + dx, y + dy)))
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    portals.sort();
+
+    let enter = portals[0].1;
+    let exit = portals.pop().unwrap().1;
+
+    let mut links = HashMap::new();
+    for c in portals[1..].chunks_exact(2) {
+        links.insert(c[0].2, c[1].1);
+        links.insert(c[1].2, c[0].1);
+    }
+
+    let mut seen = HashSet::new();
+    let mut todo = VecDeque::new();
+    todo.push_back((enter.0, enter.1, 0));
+    while let Some((x, y, steps)) = todo.pop_front() {
+        if (x, y) == exit {
+            println!("Part 1: {}", steps);
+            break;
+        }
+        if seen.contains(&(x, y)) {
+            continue;
+        }
+        seen.insert((x, y));
+        let c = get(x, y);
+        if char::is_uppercase(c) {
+            if let Some(w) = links.get(&(x, y)) {
+                todo.push_back((w.0, w.1, steps));
+            }
+        } else if c != '#' {
+            // Take new steps
+            for (dx, dy) in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                todo.push_back((x + dx, y + dy, steps + 1));
+            }
+        }
+    }
+}
