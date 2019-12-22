ca9abcfe612c9b9f3e6824b25530ec92598a5798
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-22 12:31:14 -0500

    Simplifying, still use WA for Part 2

--- 2019/22/src/main.rs
+++ 2019/22/src/main.rs
@@ -1,28 +1,25 @@
-use std::collections::HashMap;
 use std::io::BufRead;
 use std::str::FromStr;
 
+// Represents T.0*x + T.1
+type Transform = (i128, i128);
+
 #[derive(Debug)]
 enum Action {
-    CutPos(i64),
-    CutNeg(i64),
-    DealIncrement(i64),
+    Cut(i128),
+    DealIncrement(i128),
     DealStack(),
 }
 
 impl Action {
     fn new(s: &str) -> Action {
         let i = s.split(' ')
-            .filter_map(|i| i64::from_str(i).ok())
-            .collect::<Vec<i64>>();
+            .filter_map(|i| i128::from_str(i).ok())
+            .next().unwrap_or(0);
         if s.starts_with("cut") {
-            if i[0] < 0 {
-                Action::CutNeg(-i[0])
-            } else {
-                Action::CutPos(i[0])
-            }
+            Action::Cut(i)
         } else if s.starts_with("deal with increment") {
-            Action::DealIncrement(i[0])
+            Action::DealIncrement(i)
         } else if s == "deal into new stack" {
             Action::DealStack()
         } else {
@@ -30,35 +27,20 @@ impl Action {
         }
     }
 
-    // returns A, B such that A*c + B is the new position
-    fn math(&self, deck_size: i64) -> (i64, i64) {
+    // returns A, B such that A*i + B is the new position
+    fn math(&self, deck_size: i128) -> Transform {
         match self {
-            Action::CutPos(i) => {
-                (1, -*i)
-            },
-            Action::CutNeg(i) => {
-                (1, deck_size + *i)
-            }
-            Action::DealIncrement(i) => {
-                (*i, 0)
-            }
-            Action::DealStack() => {
-                (-1, deck_size - 1)
-            }
+            Action::Cut(i) => (1, -*i),
+            Action::DealIncrement(i) => (*i, 0),
+            Action::DealStack() => (-1, deck_size - 1),
         }
     }
 }
 
-fn fuse((a, b): (i64, i64), (c, d): (i64, i64), deck_size: i64) -> (i64, i64) {
-    let mut a = c as i128 * a as i128;
-    let mut b = c as i128 * b as i128 + d as i128;
-    while a < 0 {
-        a += deck_size as i128;
-    }
-    while b < 0 {
-        b += deck_size as i128;
-    }
-    ((a % deck_size as i128) as i64, (b % deck_size as i128) as i64)
+fn fuse((a, b): Transform, (c, d): Transform, deck_size: i128) -> Transform {
+    let a = c * a;
+    let b = c * b + d;
+    (a.rem_euclid(deck_size), b.rem_euclid(deck_size))
 }
 
 fn main() {
@@ -67,20 +49,20 @@ fn main() {
         .map(|line| Action::new(&line.unwrap()))
         .collect::<Vec<Action>>();
 
-    let build = |deck_size: i64| {
+    let build = |deck_size: i128| {
         actions.iter()
             .fold((1, 0), |acc, action|
                   fuse(acc, action.math(deck_size), deck_size))
     };
 
-    let deck_size: i64 = 10007;
+    let deck_size: i128 = 10007;
     let t = build(deck_size);
     println!("Part 1: {}", (t.0 * 2019 + t.1) % deck_size);
 
     ////////////////////////////////////////////////////////////////////////////////
 
-    let deck_size: i64 = 119315717514047;
-    let num_passes: i64 = 101741582076661;
+    let deck_size: i128 = 119315717514047;
+    let num_passes: i128 = 101741582076661;
     let mut t = build(deck_size);
 
     let mut i = 1;
@@ -92,12 +74,11 @@ fn main() {
     }
 
     let mut t = (1, 0);
-    let mut total: usize = 0;
     for (i, p) in v.iter().enumerate() {
         if (num_passes & (1 << i)) != 0 {
             t = fuse(t, *p, deck_size);
-            total += (1 << i);
         }
     }
-    println!("Part 2: Ask Wolfram Alpha to solve ({} * i + {}) % {} = 0", t.0, t.1 - 2020, deck_size);
+    println!("Part 2: Ask Wolfram Alpha to solve ({} * i + {}) % {} = 0",
+             t.0, t.1 - 2020, deck_size);
 }
