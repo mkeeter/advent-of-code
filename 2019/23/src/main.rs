4a6f59c71e213d7bd2faf0f02262fbd57dc0caa5
Matt Keeter <matt.j.keeter@gmail.com>
2019-12-23 08:32:31 -0500

    2019, day 23, part 2

--- 2019/23/src/main.rs
+++ 2019/23/src/main.rs
@@ -1,26 +1,52 @@
+use std::collections::HashSet;
 use std::io::Read;
 use std::str::FromStr;
 
 use vm::Vm;
 
+const NUM_MACHINES: usize = 50;
+
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
 
-    let mut vms = vec![Vm::from_str(&input).unwrap(); 50];
+    let mut nat: Option<(i64, i64)> = None;
+    let mut idle = [0; NUM_MACHINES];
+    let mut seen = HashSet::new();
+
+    let mut vms = vec![Vm::from_str(&input).unwrap(); NUM_MACHINES];
     for (i, vm) in vms.iter_mut().enumerate() {
         vm.input(i as i64);
     }
-    'outer: loop {
+    loop {
+        // The network is idle if every machine has tried to receive
+        // two packets without sending anything.
+        if idle.iter().all(|i| *i > 1) {
+            let nat = nat.unwrap();
+            if seen.contains(&nat.1) {
+                println!("Part 2: {}", nat.1);
+                break;
+            } else {
+                seen.insert(nat.1);
+            }
+            vms[0].input(nat.0);
+            vms[0].input(nat.1);
+            idle = [0; NUM_MACHINES];
+        }
         for i in 0..vms.len() {
+            idle[i] += vms[i].needs_input() as i64;
             if let Some(addr) = vms[i].step_with(-1) {
+                idle[i] = 0;
+
                 let x = vms[i].run_until().unwrap();
                 let y = vms[i].run_until().unwrap();
-                println!("Sending packet {} {} {}", addr, x, y);
                 if addr == 255 {
-                    println!("Part 1: {}", y);
-                    break 'outer;
+                    if nat.is_none() {
+                        println!("Part 1: {}", y);
+                    }
+                    nat = Some((x, y))
                 } else {
+                    idle[addr as usize] = 0;
                     vms[addr as usize].input(x);
                     vms[addr as usize].input(y);
                 }
