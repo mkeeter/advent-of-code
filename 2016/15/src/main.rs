4e76a600fa4b733760f973f8d20a439ab245f502
Matt Keeter <matt.j.keeter@gmail.com>
2020-01-10 21:54:47 -0500

    2016, day 15

--- 2016/15/src/main.rs
+++ 2016/15/src/main.rs
@@ -1,3 +1,29 @@
+use std::str::FromStr;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = std::io::stdin().lock()
+        .lines()
+        .map(|line| {
+            let ints = line.unwrap()
+                .replace('.', "")
+                .split(' ')
+                .filter_map(|w| usize::from_str(w).ok())
+                .collect::<Vec<_>>();
+            (ints[0], ints[1])
+        })
+        .collect::<Vec<(usize, usize)>>();
+
+    let part1 = (0..).find(|t| input.iter()
+            .enumerate()
+            .all(|(i, (pos, offset))| (offset + t + i + 1) % pos == 0))
+        .unwrap();
+    println!("Part 1: {}", part1);
+
+    input.push((11, 0));
+    let part2 = (0..).find(|t| input.iter()
+            .enumerate()
+            .all(|(i, (pos, offset))| (offset + t + i + 1) % pos == 0))
+        .unwrap();
+    println!("Part 2: {}", part2);
 }
