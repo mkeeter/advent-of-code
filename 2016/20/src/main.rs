abb6e7b22104d8dcc817e37459ef0554dafe7760
Matt Keeter <matt.j.keeter@gmail.com>
2020-01-11 11:48:50 -0500

    2019, day 20, part 1 (and slow part 2)

--- 2016/20/src/main.rs
+++ 2016/20/src/main.rs
@@ -1,3 +1,24 @@
+use std::str::FromStr;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let mut blacklist = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let mut itr = line
+                .split('-')
+                .filter_map(|i| usize::from_str(i).ok());
+            (itr.next().unwrap(), itr.next().unwrap())
+        })
+        .collect::<Vec<(usize, usize)>>();
+    blacklist.sort();
+
+    let allowed = |i: usize| -> bool {
+        blacklist.iter().all(|&(min, max)| i < min || i > max)
+    };
+
+    println!("Part 1: {}", (0..).find(|i| allowed(*i)).unwrap());
+    println!("Part 2: {}", (0..=4294967295).filter(|i| allowed(*i)).count());
 }
