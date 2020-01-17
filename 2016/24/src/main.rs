da0763bceb8a60bde137b98848afe1f68b3a1cad
Matt Keeter <matt.j.keeter@gmail.com>
2020-01-17 17:35:04 -0500

    2016, day 24

--- 2016/24/src/main.rs
+++ 2016/24/src/main.rs
@@ -1,3 +1,85 @@
+use std::io::BufRead;
+use std::collections::{HashSet, HashMap, VecDeque};
+
+use itertools::*;
+
+fn search(tiles: &HashSet<(usize, usize)>,
+          targets: &HashMap<(usize, usize), char>,
+          start: (usize, usize)) -> HashMap<char, usize>
+{
+    let mut todo = VecDeque::new();
+    let mut seen = HashSet::new();
+
+    let mut out = HashMap::new();
+
+    todo.push_back((start.0, start.1, 0));
+    while let Some((x, y, steps)) = todo.pop_front() {
+        if seen.contains(&(x, y)) || !tiles.contains(&(x, y)) {
+            continue;
+        } else {
+            seen.insert((x, y));
+        }
+
+        if let Some(c) = targets.get(&(x, y)) {
+            out.insert(*c, steps);
+        }
+
+        todo.push_back((x + 1, y, steps + 1));
+        todo.push_back((x - 1, y, steps + 1));
+        todo.push_back((x, y + 1, steps + 1));
+        todo.push_back((x, y - 1, steps + 1));
+    }
+    out
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut tiles = HashSet::new();
+    let mut targets = HashMap::new();
+    for (y, line) in std::io::stdin().lock().lines().enumerate() {
+        for (x, c) in line.unwrap().chars().enumerate() {
+            if c != '#' {
+                tiles.insert((x, y));
+                if c != '.' {
+                    targets.insert((x, y), c);
+                }
+            }
+        }
+    }
+
+    let mut distances: HashMap<(char, char), usize> = HashMap::new();
+    for (pos, c) in targets.iter() {
+        for (t, d) in search(&tiles, &targets, *pos) {
+            if t == *c {
+                continue;
+            } else {
+                distances.insert((*c, t), d);
+            }
+        }
+    }
+
+    let p1 = targets.values()
+        .filter(|c| **c != '0')
+        .permutations(targets.len() - 1)
+        .map(|s| {
+            distances[&('0', *s[0])] +
+            s.windows(2)
+                .map(|w| distances[&(*w[0], *w[1])])
+                .sum::<usize>() })
+        .min()
+        .unwrap();
+    println!("Part 1: {}", p1);
+
+    let p2 = targets.values()
+        .filter(|c| **c != '0')
+        .permutations(targets.len() - 1)
+        .map(|s| {
+            distances[&('0', *s[0])] +
+            s.windows(2)
+                .map(|w| distances[&(*w[0], *w[1])])
+                .sum::<usize>() +
+            distances[&(*s[s.len() - 1], '0')] })
+        .min()
+        .unwrap();
+    println!("Part 2: {}", p2);
+
 }
