c57f83c0ed62c2fb4846510e2fe6af071b434b74
Matt Keeter <matt.j.keeter@gmail.com>
2020-01-18 12:29:29 -0500

    Finish up Rust implementation for 2015

---
+++ 2015/01/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let itr = || input.chars()
+        .map(|c| match c {
+            '(' =>  1,
+            ')' => -1,
+             _  =>  0,
+        });
+
+    let p1: i32 = itr().sum();
+    println!("Part 1: {}", p1);
+
+    let p2: usize = itr()
+        .scan(0, |state, i| {
+            *state += i;
+            Some(*state)
+        })
+        .enumerate()
+        .find(|(_i, floor)| *floor < 0)
+        .unwrap().0 + 1;
+    println!("Part 2: {}", p2);
+}
