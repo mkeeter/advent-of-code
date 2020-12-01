f8dd98a1fed6ff8c7711a7cc198dfef8563fefc9
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-01 08:19:35 -0500

    Remove all explicit loops, just for fun

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -1,30 +1,25 @@
 use std::io::BufRead;
 use std::str::FromStr;
-use std::collections::{HashSet, HashMap};
+use std::collections::HashMap;
 
-fn main() -> () {
+fn main() {
     let nums = std::io::stdin().lock().lines()
         .map(|line| i32::from_str(&line.unwrap()))
         .map(|res| res.expect("Could not parse int"))
-        .collect::<HashSet<_>>();
+        .collect::<Vec<_>>();
 
-    for a in nums.iter() {
-        if nums.contains(&(2020 - a)) {
-            println!("Part 1: {}", a * (2020 - a));
-            break;
-        }
-    }
+    // Build a map from sum -> product for all pairs in the input
+    let sums: HashMap<i32, i32> = nums.iter()
+        .flat_map(|a| nums.iter().map(move |b| (a + b, a * b)))
+        .collect();
 
-    let mut sums: HashMap<i32, i32> = HashMap::new();
-    for (i, a) in nums.iter().enumerate() {
-        for b in nums.iter().skip(i + 1) {
-            sums.insert(a + b, a * b);
-        }
-    }
-    for a in nums.iter() {
-        if let Some(b) = sums.get(&(2020 - a)) {
-            println!("Part 2: {}", a * b);
-            break;
-        }
-    }
+    // The solution to part 1 is just stored in the table:
+    println!("Part 1: {}", sums.get(&2020).expect("Could not find sum"));
+
+    // Do an O(n) sweep through the table to find the triple
+    let b = nums.iter()
+        .flat_map(|a| sums.get(&(2020 - a)).map(|b| a * b))
+        .next()
+        .expect("Could not find three-item match");
+    println!("Part 2: {}", b);
 }
