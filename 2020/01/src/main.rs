c6b41fd377a4236f6308f96c5e925bb0c6d0d9c6
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-01 08:08:04 -0500

    2020, day 1

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -1,11 +1,30 @@
-use std::io::Read;
-use anyhow::Result;
+use std::io::BufRead;
+use std::str::FromStr;
+use std::collections::{HashSet, HashMap};
 
-fn main() -> Result<()> {
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)?;
+fn main() -> () {
+    let nums = std::io::stdin().lock().lines()
+        .map(|line| i32::from_str(&line.unwrap()))
+        .map(|res| res.expect("Could not parse int"))
+        .collect::<HashSet<_>>();
 
-    println!("Got input: {}", input);
+    for a in nums.iter() {
+        if nums.contains(&(2020 - a)) {
+            println!("Part 1: {}", a * (2020 - a));
+            break;
+        }
+    }
 
-    Ok(())
+    let mut sums: HashMap<i32, i32> = HashMap::new();
+    for (i, a) in nums.iter().enumerate() {
+        for b in nums.iter().skip(i + 1) {
+            sums.insert(a + b, a * b);
+        }
+    }
+    for a in nums.iter() {
+        if let Some(b) = sums.get(&(2020 - a)) {
+            println!("Part 2: {}", a * b);
+            break;
+        }
+    }
 }
