18063fe080977d15bd5ac74917ffe83be87075ac
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-08 08:19:58 -0500

    Refactor program into struct

--- 2020/08/src/main.rs
+++ 2020/08/src/main.rs
@@ -1,6 +1,5 @@
 use std::io::BufRead;
 use std::str::FromStr;
-use std::collections::HashSet;
 
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
@@ -10,41 +9,61 @@ enum Opcode {
     jmp,
 }
 
-fn parse(s: &str) -> (Opcode, i64) {
-    let mut itr = s.split(" ");
+struct Program(Vec<(Opcode, i64)>);
+impl Program {
+    fn parse_line(s: &str) -> (Opcode, i64) {
+        let mut itr = s.split(" ");
 
-    let op_str = itr.next().unwrap();
-    let op = match op_str {
-        "nop" => Opcode::nop,
-        "acc" => Opcode::acc,
-        "jmp" => Opcode::jmp,
-        _ => panic!("Unknown opcode {}", op_str),
-    };
+        let op_str = itr.next().unwrap();
+        let op = match op_str {
+            "nop" => Opcode::nop,
+            "acc" => Opcode::acc,
+            "jmp" => Opcode::jmp,
+            _ => panic!("Unknown opcode {}", op_str),
+        };
 
-    let num = i64::from_str(itr.next().unwrap()).unwrap();
+        let num = i64::from_str(itr.next().unwrap()).unwrap();
 
-    (op, num)
-}
+        (op, num)
+    }
 
-fn main() {
-    let program = std::io::stdin().lock().lines()
-        .map(|line| parse(&line.unwrap()))
-        .collect::<Vec<_>>();
-    println!("{:?}", program);
-
-    let step = |(ip, ac) : (i64, i64)| {
-        let (op, v) = &program[ip as usize];
+    fn step(&self, ip: i64, ac: i64) -> (i64, i64) {
+        let (op, v) = &self.0[ip as usize];
         match op {
             Opcode::nop => (ip + 1, ac),
             Opcode::acc => (ip + 1, v + ac),
             Opcode::jmp => (ip + v, ac),
         }
-    };
+    }
 
-    let mut seen = HashSet::new();
-    let p1 = std::iter::successors(Some((0, 0)), |i| Some(step(*i)))
-        .find(move |(ip, _)| !seen.insert(*ip))
-        .unwrap();
-    println!("{:?}", p1);
+    // Returns Okay(ac) if the program terminates, and Err(ac) if not
+    fn run(&self) -> Result<i64, i64> {
+        let mut seen = vec![false; self.0.len()];
+        let mut ip = 0;
+        let mut ac = 0;
+        loop {
+            let ip_ = ip as usize;
+            if ip_ == self.0.len() + 1 {
+                return Ok(ac);
+            } else if seen[ip_] {
+                return Err(ac);
+            } else {
+                seen[ip_] = true;
+            }
+            let next = self.step(ip, ac);
+            ip = next.0;
+            ac = next.1;
+        }
+    }
+}
+
+
+fn main() {
+    let p = Program(std::io::stdin().lock().lines()
+        .map(|line| Program::parse_line(&line.unwrap()))
+        .collect::<Vec<_>>());
+
+    let p1 = p.run().unwrap_err();
+    println!("Part 1: {}", p1);
 
 }
