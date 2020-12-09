789e3792fdbd21f003a02c6018c0ee8df57f2c58
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-09 08:01:03 -0500

    2020, day 9

---
+++ 2020/09/src/main.rs
@@ -0,0 +1,40 @@
+use std::io::BufRead;
+use std::str::FromStr;
+
+const LOOKBACK: usize = 25;
+
+fn main() {
+    let p = std::io::stdin().lock().lines()
+        .map(|line| u64::from_str(&line.unwrap()).unwrap())
+        .collect::<Vec<u64>>();
+
+    let mut buf = [0; LOOKBACK];
+    buf.copy_from_slice(&p[0..LOOKBACK]);
+
+    let mut target = 0;
+    for (i,n) in p[LOOKBACK..].iter().enumerate() {
+        if buf.iter().enumerate().any(|(j, a)| buf[j..].iter()
+                                 .any(|b| a + b == *n)) {
+            buf[i % LOOKBACK] = *n;
+        } else {
+            target = *n;
+            println!("Part 1: {}", target);
+            break;
+        }
+    }
+
+    'outer: for i in 0..p.len() {
+        let mut csum = p[i];
+        for j in (i + 1)..p.len() {
+            csum += p[j];
+            if csum > target {
+                break;
+            } else if csum == target {
+                let min = p[i..=j].iter().min().unwrap();
+                let max = p[i..=j].iter().max().unwrap();
+                println!("Part 2: {}", min + max);
+                break 'outer;
+            }
+        }
+    }
+}
