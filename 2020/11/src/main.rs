9cdd3bae9aee45f2cc8487630c5a30b07edef2e2
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-11 13:05:11 -0500

    Much cleaner implementation for 2020, day 11

--- 2020/11/src/main.rs
+++ 2020/11/src/main.rs
@@ -2,72 +2,63 @@ use std::collections::HashMap;
 use std::io::Read;
 use arrayvec::ArrayVec;
 
+fn run<F>(ns: &Vec<ArrayVec::<[usize; 8]>>, pred: F) -> usize
+    where F: Fn(bool, usize) -> bool
+{
+    let mut state = vec![true; ns.len()];
+    let mut next = vec![false; ns.len()];
+    while state != next {
+        std::mem::swap(&mut state, &mut next);
+        for (i, n) in ns.iter().enumerate() {
+            next[i] = pred(state[i], n.iter().filter(|j| state[**j]).count());
+        }
+    }
+    state.iter().filter(|j| **j).count()
+}
+
+fn table(chairs: &HashMap<(i32, i32), usize>, max_dist: i32)
+    -> Vec<ArrayVec::<[usize; 8]>>
+{
+    const NEIGHBORS: [(i32, i32); 8] = [(-1, -1), (-1, 0), (-1, 1),
+                                        ( 0, -1), /* 0, */ ( 0, 1),
+                                        ( 1, -1), ( 1, 0), ( 1, 1)];
+    chairs.iter().map(|((x, y), _)|
+        NEIGHBORS.iter()
+            .filter_map(|(dx, dy)| (1..=max_dist)
+                .filter_map(|i| chairs.get(&(x + i*dx, y + i*dy)))
+                .next())
+            .copied()
+            .collect())
+        .collect()
+}
+
 fn main() {
     // Build a map from chairs to positions in a Vec
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).expect("Failed to read input");
-    let chairs: HashMap<(i32, i32), usize> = input.lines().enumerate()
+
+    // Unpack into a list of chairs
+    let mut chairs: HashMap<(i32, i32), usize> = input.lines().enumerate()
         .flat_map(|(y, line)| line.chars().enumerate()
             .filter(|(_, c)| *c == 'L')
             .map(move |(x, _)| (x as i32, y as i32)))
-        .zip(0..)
+        .zip(std::iter::repeat(0))
         .collect();
 
-    const NEIGHBORS: [(i32, i32); 8] = [(-1, -1), (-1, 0), (-1, 1),
-                                        ( 0, -1), /* 0, */ ( 0, 1),
-                                        ( 1, -1), ( 1, 0), ( 1, 1)];
+    // Assign indexes based on iteration order
+    chairs.iter_mut().enumerate().for_each(|(i, c)| *c.1 = i);
+    let chairs = chairs; // lock mutability
 
-    let mut ns: Vec<ArrayVec<[usize; 8]>> = Vec::new();
-    for ((x, y), i) in chairs.iter() {
-        let mut arr = ArrayVec::new();
-        for n in NEIGHBORS.iter()
-            .filter_map(|(dx, dy)| chairs.get(&(x + dx, y + dy)))
-        {
-            arr.push(*n);
-        }
-        ns.push(arr);
-    }
-    println!("{:?}", ns);
-    /*
+    // Build a list of neighbors checked by each chair, then solve
+    let ns = table(&chairs, 1);
+    let p1 = run(&ns, |filled, count| (filled && count < 4) || (count == 0));
+    println!("Part 1: {}", p1);
 
-    let mut chairs = input.clone();
-    let mut next = HashMap::new();
-    let mut changed = true;
-    while changed {
-        changed = false;
-        for ((x, y), filled) in chairs.iter() {
-            let count = NEIGHBORS.iter()
-                .filter_map(|(dx, dy)| chairs.get(&(x + dx, y + dy)))
-                .filter(|f| **f)
-                .count();
-
-            let val = (*filled && !(count >= 4)) ||
-                      (!*filled && count == 0);
-            changed |= val != *filled;
-            next.insert((*x, *y), val);
-        }
-        std::mem::swap(&mut chairs, &mut next);
-    }
-    println!("Part 1: {}", chairs.iter().filter(|(_, v)| **v).count());
-
-    let mut changed = true;
-    let mut chairs = input.clone();
-    let mut next = HashMap::new();
-    while changed {
-        changed = false;
-        for ((x, y), filled) in chairs.iter() {
-            let count = NEIGHBORS.iter()
-                .filter_map(|(dx, dy)| (1..150).filter_map(|i| chairs.get(&(x + i*dx, y + i*dy))).next())
-                .filter(|f| **f)
-                .count();
-
-            let val = (*filled && !(count >= 5)) ||
-                      (!*filled && count == 0);
-            changed |= val != *filled;
-            next.insert((*x, *y), val);
-        }
-        std::mem::swap(&mut chairs, &mut next);
-    }
-    println!("Part 2: {}", chairs.iter().filter(|(_, v)| **v).count());
-    */
+    // Calculate the longest possible distance to travel
+    let max = input.lines().map(|line| line.chars().count())
+        .chain(std::iter::once(input.lines().count()))
+        .max().unwrap() as i32;
+    let ns = table(&chairs, max);
+    let p2 = run(&ns, |filled, count| (filled && count < 5) || (count == 0));
+    println!("Part 2: {}", p2);
 }
