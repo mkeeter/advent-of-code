c4bcb889998209eb48b3275e16f6897914ced1b6
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-12 09:42:26 -0500

    Simplify core loop into a function

--- 2020/12/src/main.rs
+++ 2020/12/src/main.rs
@@ -1,5 +1,24 @@
 use std::io::BufRead;
 
+fn run<F>(input: &Vec<(char, i64)>, mut dir: (i64, i64), f: F) -> i64
+    where F: Fn(char, (i64, i64), (i64, i64)) -> ((i64, i64), (i64, i64))
+{
+    let mut pos: (i64, i64) = (0, 0);
+    for (cmd, count) in input.iter() {
+        let n = match cmd {
+            'N'|'S'|'E'|'W'|'F' => *count,
+            'R'|'L' => count / 90,
+            _ => panic!("Invalid command {}", cmd),
+        };
+        for _i in 0..n {
+            let next = f(*cmd, pos, dir);
+            pos = next.0;
+            dir = next.1;
+        }
+    }
+    pos.0.abs() + pos.1.abs()
+}
+
 fn main() {
     let input: Vec<(char, i64)> = std::io::stdin().lock().lines()
         .map(|line| {
@@ -9,49 +28,27 @@ fn main() {
         })
         .collect();
 
-    let mut pos: (i64, i64) = (0, 0);
-    let mut dir = (1, 0);
-    for (cmd, count) in input.iter() {
-        let n = match cmd {
-            'N'|'S'|'E'|'W'|'F' => *count,
-            'R'|'L' => count / 90,
-            _ => panic!("Invalid command {}", cmd),
-        };
-        for _i in 0..n {
-            match cmd {
-                'N' => pos.1 += 1,
-                'S' => pos.1 -= 1,
-                'E' => pos.0 += 1,
-                'W' => pos.0 -= 1,
-                'F' => { pos.0 += dir.0; pos.1 += dir.1 },
-                'L' => dir = (-dir.1,  dir.0),
-                'R' => dir = ( dir.1, -dir.0),
-                _ => panic!("Invalid command {}", cmd),
-            }
-        }
-    }
-    println!("Part 1: {}", pos.0.abs() + pos.1.abs());
+    let p1 = |cmd, pos: (i64, i64), dir: (i64, i64)| match cmd {
+        'N' => ((pos.0, pos.1 + 1), dir),
+        'S' => ((pos.0, pos.1 - 1), dir),
+        'E' => ((pos.0 + 1, pos.1), dir),
+        'W' => ((pos.0 - 1, pos.1), dir),
+        'F' => ((pos.0 + dir.0, pos.1 + dir.1), dir),
+        'L' => (pos, (-dir.1,  dir.0)),
+        'R' => (pos, ( dir.1, -dir.0)),
+        _ => panic!("Invalid command {}", cmd),
+    };
+    println!("Part 1: {}", run(&input, (1, 0), p1));
 
-    let mut pos: (i64, i64) = (0, 0);
-    let mut way = (10, 1);
-    for (cmd, count) in input.iter() {
-        let n = match cmd {
-            'N'|'S'|'E'|'W'|'F' => *count,
-            'R'|'L' => count / 90,
-            _ => panic!("Invalid command {}", cmd),
-        };
-        for _i in 0..n {
-            match cmd {
-                'N' => way.1 += 1,
-                'S' => way.1 -= 1,
-                'E' => way.0 += 1,
-                'W' => way.0 -= 1,
-                'F' => { pos.0 += way.0; pos.1 += way.1 },
-                'L' => way = (-way.1,  way.0),
-                'R' => way = ( way.1, -way.0),
-                _ => panic!("Invalid command {}", cmd),
-            }
-        }
-    }
-    println!("Part 2: {}", pos.0.abs() + pos.1.abs());
+    let p2 = |cmd, pos: (i64, i64), dir: (i64, i64)| match cmd {
+        'N' => (pos, (dir.0, dir.1 + 1)),
+        'S' => (pos, (dir.0, dir.1 - 1)),
+        'E' => (pos, (dir.0 + 1, dir.1)),
+        'W' => (pos, (dir.0 - 1, dir.1)),
+        'F' => ((pos.0 + dir.0, pos.1 + dir.1), dir),
+        'L' => (pos, (-dir.1,  dir.0)),
+        'R' => (pos, ( dir.1, -dir.0)),
+        _ => panic!("Invalid command {}", cmd),
+    };
+    println!("Part 2: {}", run(&input, (10, 1), p2));
 }
