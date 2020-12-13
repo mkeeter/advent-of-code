e521c0663b74d2f336b1995d6c10fe0cb01119ee
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-13 10:47:50 -0500

    Simplify parsing

--- 2020/13/src/main.rs
+++ 2020/13/src/main.rs
@@ -5,33 +5,32 @@ fn main() {
     let mut iter = stdin.lock().lines();
     let t: i64 = iter.next().unwrap().unwrap().parse().unwrap();
 
-    let buses = iter.next().unwrap().unwrap();
+    let buses = iter.next().unwrap().unwrap()
+        .split(",")
+        .enumerate()
+        .filter_map(|(i, s)| s.parse().ok().map(|b| (i as i64, b)))
+        .map(|(i, b): (i64, i64)| (b, (b - i).rem_euclid(b)))
+        .collect::<Vec<_>>();
 
-    let p1 = buses.split(",").filter_map(|s| s.parse().ok())
-        .map(|i: i64| ((i - t).rem_euclid(i), i))
+    let p1 = buses.iter()
+        .map(|i| ((i.0 - t).rem_euclid(i.0), i.0))
         .min()
         .unwrap();
     println!("Part 1: {}", p1.0 * p1.1);
-
-    let p2 = buses.split(",").enumerate()
-        .filter_map(|(i, s)| s.parse().ok().map(|b| (i, b)))
-        .map(|(i, b): (usize, i64)| (b, (b - (i as i64)).rem_euclid(b)))
-        .collect::<Vec<(i64, i64)>>();
-    println!("{:?}", p2);
 
     let mut d = 1;
     let mut t = 0;
     let mut i = 0;
     let mut m: Option<i64> = None;
     loop {
-        if t % p2[i].0 == p2[i].1 {
+        if t % buses[i].0 == buses[i].1 {
             if let Some(n) = m {
                 i += 1;
                 d = t - n;
                 m = None;
             } else {
                 m = Some(t);
-                if i == p2.len() - 1 {
+                if i == buses.len() - 1 {
                     break;
                 }
             }
