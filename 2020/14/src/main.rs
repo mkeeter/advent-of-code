a4b4b84d42867f69b09961e12b7c3003d3d30e6d
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-14 07:14:32 -0500

    Hack my way to 2020, day 14, part 2

--- 2020/14/src/main.rs
+++ 2020/14/src/main.rs
@@ -2,8 +2,25 @@ use std::collections::HashMap;
 use std::io::BufRead;
 use regex::Regex;
 
+fn unpack(mask: &[(usize, char)], addr: u64, out: &mut Vec<u64>)
+{
+    match mask.get(0) {
+        None => out.push(addr),
+        Some((i, c)) => match c {
+            '1' => unpack(&mask[1..], addr | (1 << (35 - i)), out),
+            '0' => unpack(&mask[1..], addr, out),
+            'X' => {
+                unpack(&mask[1..], addr | (1 << (35 - i)), out);
+                unpack(&mask[1..], addr & (!(1 << (35 - i))), out);
+            },
+            _ => panic!("Invalid mask char: {}", c),
+        }
+    }
+}
+
 fn main() {
     let stdin = std::io::stdin();
+    let lines: Vec<String> = stdin.lock().lines().map(|s| s.unwrap()).collect();
 
     let imem = Regex::new(r#"^mem\[([0-9]+)\] = ([0-9]+)$"#).unwrap();
     let imask = Regex::new(r#"^mask = ([01X]+)$"#).unwrap();
@@ -12,8 +29,7 @@ fn main() {
     let mut mask_set: u64 = 0;
     let mut mask_clear: u64 = 0;
 
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
+    for line in lines.iter() {
         if let Some(c) = imask.captures(&line) {
             mask_set = 0;
             mask_clear = 0;
@@ -36,4 +52,24 @@ fn main() {
     }
     let sum: u64 = mem.iter().map(|(_k, v)| *v).sum();
     println!("Part 1: {}", sum);
+
+    let mut mem = HashMap::new();
+    let mut mask = "";
+    for line in lines.iter() {
+        if let Some(c) = imask.captures(&line) {
+            mask = c.get(1).unwrap().as_str();
+            assert!(mask.len() == 36);
+        } else if let Some(c) = imem.captures(&line) {
+            let addr: u64 = c.get(1).unwrap().as_str().parse().unwrap();
+            let val: u64 = c.get(2).unwrap().as_str().parse().unwrap();
+            let mut out = Vec::new();
+            let mask: Vec<_> = mask.chars().enumerate().collect();
+            unpack(&mask, addr, &mut out);
+            for addr in out {
+                mem.insert(addr, val);
+            }
+        }
+    }
+    let sum: u64 = mem.iter().map(|(_k, v)| *v).sum();
+    println!("Part 2: {}", sum);
 }
