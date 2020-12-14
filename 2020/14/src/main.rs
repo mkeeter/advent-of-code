da10fe8a66d501121fd77e2c342274ce9f45007b
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-14 09:52:20 -0500

    Totally clean up 2020, day 14

--- 2020/14/src/main.rs
+++ 2020/14/src/main.rs
@@ -2,74 +2,82 @@ use std::collections::HashMap;
 use std::io::BufRead;
 use regex::Regex;
 
-fn unpack(mask: &[(usize, char)], addr: u64, out: &mut Vec<u64>)
+type Memory = HashMap<u64, u64>;
+fn run<F, G, T>(lines: &[String], decode: F, write: G) -> u64
+    where F: Fn(&str) -> T,
+          G: Fn(u64, u64, &T, &mut Memory),
 {
-    match mask.get(0) {
-        None => out.push(addr),
-        Some((i, c)) => match c {
-            '1' => unpack(&mask[1..], addr | (1 << (35 - i)), out),
-            '0' => unpack(&mask[1..], addr, out),
-            'X' => {
-                unpack(&mask[1..], addr | (1 << (35 - i)), out);
-                unpack(&mask[1..], addr & (!(1 << (35 - i))), out);
-            },
+    let imem = Regex::new(r#"^mem\[([0-9]+)\] = ([0-9]+)$"#).unwrap();
+    let imask = Regex::new(r#"^mask = ([01X]+)$"#).unwrap();
+
+    let mut mem = HashMap::new();
+
+    let mut mask = None;
+    for line in lines.iter() {
+        if let Some(c) = imask.captures(&line) {
+            mask = Some(decode(c.get(1).unwrap().as_str()));
+        } else if let Some(c) = imem.captures(&line) {
+            let addr = c.get(1).unwrap().as_str().parse().unwrap();
+            let val = c.get(2).unwrap().as_str().parse().unwrap();
+            write(val, addr, mask.as_ref().unwrap(), &mut mem);
+        } else {
+            panic!("Failed to parse: {}", line);
+        }
+    }
+    mem.iter().map(|(_k, v)| *v).sum()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn decode_set_clear(s: &str) -> (u64, u64) {
+    let mut set = 0;
+    let mut clear = 0;
+    for (i, c) in s.chars().enumerate() {
+        match c {
+            '1' => set |= 1 << (35 - i),
+            '0' => clear |= 1 << (35 - i),
+            'X' => continue,
             _ => panic!("Invalid mask char: {}", c),
         }
     }
+    (set, clear)
 }
+
+fn write_set_clear(val: u64, addr: u64, (set, clear): &(u64, u64),
+                   mem: &mut Memory) {
+    mem.insert(addr, (val | set) & (!clear));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn decode_multi(s: &str) -> Vec<(usize, char)> {
+    s.chars().enumerate().collect()
+}
+
+fn write_multi<D: ?Sized>(val: u64, addr: u64, mask: &D, mem: &mut Memory)
+    where D: AsRef<[(usize, char)]>
+{
+    let mask = mask.as_ref();
+    match mask.get(0) {
+        None => { mem.insert(addr, val); },
+        Some((i, c)) => match c {
+            '0' => write_multi(val, addr, &mask[1..], mem),
+            '1' => write_multi(val, addr | (1 << (35 - i)), &mask[1..], mem),
+            'X' => {
+                write_multi(val, addr | (1 << (35 - i)), &mask[1..], mem);
+                write_multi(val, addr & (!(1 << (35 - i))), &mask[1..], mem);
+            },
+            _ => panic!("Invalid mask char: {}", c),
+        }
+    };
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 fn main() {
     let stdin = std::io::stdin();
     let lines: Vec<String> = stdin.lock().lines().map(|s| s.unwrap()).collect();
 
-    let imem = Regex::new(r#"^mem\[([0-9]+)\] = ([0-9]+)$"#).unwrap();
-    let imask = Regex::new(r#"^mask = ([01X]+)$"#).unwrap();
-
-    let mut mem = HashMap::new();
-    let mut mask_set: u64 = 0;
-    let mut mask_clear: u64 = 0;
-
-    for line in lines.iter() {
-        if let Some(c) = imask.captures(&line) {
-            mask_set = 0;
-            mask_clear = 0;
-            let m = c.get(1).unwrap().as_str();
-            for (i, c) in m.chars().enumerate() {
-                match c {
-                    '1' => mask_set |= 1 << (35 - i),
-                    '0' => mask_clear |= 1 << (35 - i),
-                    'X' => continue,
-                    _ => panic!("Invalid mask char: {}", c),
-                }
-            }
-        } else if let Some(c) = imem.captures(&line) {
-            let addr: usize = c.get(1).unwrap().as_str().parse().unwrap();
-            let val: u64 = c.get(2).unwrap().as_str().parse().unwrap();
-            mem.insert(addr, (val | mask_set) & (!mask_clear));
-        } else {
-            println!("Failed to parse: {}", line);
-        }
-    }
-    let sum: u64 = mem.iter().map(|(_k, v)| *v).sum();
-    println!("Part 1: {}", sum);
-
-    let mut mem = HashMap::new();
-    let mut mask = "";
-    for line in lines.iter() {
-        if let Some(c) = imask.captures(&line) {
-            mask = c.get(1).unwrap().as_str();
-            assert!(mask.len() == 36);
-        } else if let Some(c) = imem.captures(&line) {
-            let addr: u64 = c.get(1).unwrap().as_str().parse().unwrap();
-            let val: u64 = c.get(2).unwrap().as_str().parse().unwrap();
-            let mut out = Vec::new();
-            let mask: Vec<_> = mask.chars().enumerate().collect();
-            unpack(&mask, addr, &mut out);
-            for addr in out {
-                mem.insert(addr, val);
-            }
-        }
-    }
-    let sum: u64 = mem.iter().map(|(_k, v)| *v).sum();
-    println!("Part 2: {}", sum);
+    println!("Part 1: {}", run(&lines, decode_set_clear, write_set_clear));
+    println!("Part 2: {}", run(&lines, decode_multi, write_multi));
 }
