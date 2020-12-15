e14df22c35d64fb6a0477535ad00fd4d1aa09c07
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-15 06:52:55 -0500

    Refactor into a function

--- 2020/15/src/main.rs
+++ 2020/15/src/main.rs
@@ -1,43 +1,32 @@
 use std::io::Read;
 
+fn run(input: &[usize], n: usize) -> usize {
+    let mut history = vec![0; n];
+    let mut next = 0;
+    for i in 0..n {
+        let speak = next;
+
+        // Where wast thou when I laid the foundations of the Earth?
+        if i < input.len() {
+            next = input[i];
+        } else if history[next] == 0 {
+            next = 0;
+        } else {
+            next = i - history[next];
+        }
+
+        // Declare, if thou hast understanding.
+        history[speak] = i;
+    }
+    next
+}
+
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
 
     let input: Vec<usize> = input.split(",").map(|s| s.parse().unwrap()).collect();
 
-    let mut history = [0; 65536];
-    let mut next = 0;
-    for i in 0..2020 {
-        let speak = next;
-
-        if i < input.len() {
-            next = input[i];
-        } else if history[next] == 0 {
-            next = 0;
-        } else {
-            next = i - history[next];
-        }
-
-        history[speak] = i;
-    }
-
-    println!("{}", next);
-
-    let mut history = vec![0; 30000000];
-    let mut next = 0;
-    for i in 0..30000000 {
-        let speak = next;
-
-        if i < input.len() {
-            next = input[i];
-        } else if history[next] == 0 {
-            next = 0;
-        } else {
-            next = i - history[next];
-        }
-
-        history[speak] = i;
-    }
-    println!("{}", next);
+    println!("Part 1: {}", run(&input, 2020));
+    println!("Part 2: {}", run(&input, 30000000));
 }
