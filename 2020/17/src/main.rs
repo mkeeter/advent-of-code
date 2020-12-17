2db7537ce58998bdb7efb56b9a643d4c1fc80886
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-17 08:29:35 -0500

    Clean up code

--- 2020/17/src/main.rs
+++ 2020/17/src/main.rs
@@ -1,89 +1,78 @@
 use std::io::BufRead;
 use std::collections::HashSet;
 
+use itertools::{Itertools, iproduct};
+
 fn main() {
-    let mut input: HashSet<(i64, i64, i64)> = HashSet::new();
+    let mut input: HashSet<(i64, i64)> = HashSet::new();
     for (y, line) in std::io::stdin().lock().lines().enumerate() {
         for (x, c) in line.unwrap().chars().enumerate() {
             if c == '#' {
-                input.insert((x as i64, y as i64, 0));
+                input.insert((x as i64, y as i64));
             }
         }
     }
-    let mut grid = input.clone();
 
-    let tern = |i| match i {
-        0 => -1,
-        1 => 1,
-        2 => 0,
-        _ => panic!("Invalid tern {}", i),
-    };
+    // Convert into a 3D grid by setting z = 0;
+    let mut grid: HashSet<(i64, i64, i64)> = input.iter()
+        .map(|&(x, y)| (x, y, 0))
+        .collect();
+
+    // Consistent ordering of n-dimensional neighbors!
+    let tern = |i: usize, d: u32| [-1, 1, 0][(i / 3_usize.pow(d)) % 3];
 
     for _i in 0..6 {
-        let xmin = grid.iter().map(|p| p.0).min().unwrap();
-        let xmax = grid.iter().map(|p| p.0).max().unwrap();
-        let ymin = grid.iter().map(|p| p.1).min().unwrap();
-        let ymax = grid.iter().map(|p| p.1).max().unwrap();
-        let zmin = grid.iter().map(|p| p.2).min().unwrap();
-        let zmax = grid.iter().map(|p| p.2).max().unwrap();
+        let xm = grid.iter().map(|p| p.0).minmax().into_option().unwrap();
+        let ym = grid.iter().map(|p| p.1).minmax().into_option().unwrap();
+        let zm = grid.iter().map(|p| p.2).minmax().into_option().unwrap();
 
-        let mut next: HashSet<(i64, i64, i64)> = HashSet::new();
-        for x in (xmin - 1)..=(xmax + 1) {
-            for y in (ymin - 1)..=(ymax + 1) {
-                for z in (zmin - 1)..=(zmax + 1) {
-                    let count = (0..26).filter(|n| {
-                        let dx = tern(n % 3);
-                        let dy = tern((n / 3) % 3);
-                        let dz = tern((n / 9) % 3);
-                        grid.contains(&(x + dx, y + dy, z + dz))
-                    }).count();
-                    let now_active = (grid.contains(&(x, y, z)) && count == 2)
-                        || (count == 3);
-                    if now_active {
-                        next.insert((x, y, z));
-                    }
-                }
-            }
-        }
-        std::mem::swap(&mut grid, &mut next);
+        grid = iproduct!(
+                (xm.0 - 1)..=(xm.1 + 1),
+                (ym.0 - 1)..=(ym.1 + 1),
+                (zm.0 - 1)..=(zm.1 + 1))
+            .filter_map(|(x, y, z)| {
+                let count = (0..(3_usize.pow(3) - 1)).filter(|&n| {
+                    let dx = tern(n, 0);
+                    let dy = tern(n, 1);
+                    let dz = tern(n, 2);
+                    grid.contains(&(x + dx, y + dy, z + dz))
+                }).count();
+                Some((x, y, z)).filter(|_|
+                    (grid.contains(&(x, y, z)) && count == 2)
+                    || (count == 3))
+            })
+            .collect();
     }
     println!("{}", grid.len());
 
+    // Convert into a 4D grid by setting w = 0;
     let mut grid: HashSet<(i64, i64, i64, i64)> = input.into_iter()
-        .map(|(x, y, z)| (x, y, z, 0))
+        .map(|(x, y)| (x, y, 0, 0))
         .collect();
-    for _i in 0..6 {
-        let xmin = grid.iter().map(|p| p.0).min().unwrap();
-        let xmax = grid.iter().map(|p| p.0).max().unwrap();
-        let ymin = grid.iter().map(|p| p.1).min().unwrap();
-        let ymax = grid.iter().map(|p| p.1).max().unwrap();
-        let zmin = grid.iter().map(|p| p.2).min().unwrap();
-        let zmax = grid.iter().map(|p| p.2).max().unwrap();
-        let wmin = grid.iter().map(|p| p.3).min().unwrap();
-        let wmax = grid.iter().map(|p| p.3).max().unwrap();
 
-        let mut next: HashSet<(i64, i64, i64, i64)> = HashSet::new();
-        for x in (xmin - 1)..=(xmax + 1) {
-            for y in (ymin - 1)..=(ymax + 1) {
-                for z in (zmin - 1)..=(zmax + 1) {
-                    for w in (wmin - 1)..=(wmax + 1) {
-                        let count = (0..80).filter(|n| {
-                            let dx = tern(n % 3);
-                            let dy = tern((n / 3) % 3);
-                            let dz = tern((n / 9) % 3);
-                            let dw = tern((n / 27) % 3);
-                            grid.contains(&(x + dx, y + dy, z + dz, w + dw))
-                        }).count();
-                        let now_active = (grid.contains(&(x, y, z, w)) && count == 2)
-                            || (count == 3);
-                        if now_active {
-                            next.insert((x, y, z, w));
-                        }
-                    }
-                }
-            }
-        }
-        std::mem::swap(&mut grid, &mut next);
+    for _i in 0..6 {
+        let xm = grid.iter().map(|p| p.0).minmax().into_option().unwrap();
+        let ym = grid.iter().map(|p| p.1).minmax().into_option().unwrap();
+        let zm = grid.iter().map(|p| p.2).minmax().into_option().unwrap();
+        let wm = grid.iter().map(|p| p.3).minmax().into_option().unwrap();
+
+        grid = iproduct!(
+                (xm.0 - 1)..=(xm.1 + 1),
+                (ym.0 - 1)..=(ym.1 + 1),
+                (zm.0 - 1)..=(zm.1 + 1),
+                (wm.0 - 1)..=(wm.1 + 1))
+            .filter_map(|(x, y, z, w)| {
+                let count = (0..(3_usize.pow(4) - 1)).filter(|&n| {
+                    let dx = tern(n, 0);
+                    let dy = tern(n, 1);
+                    let dz = tern(n, 2);
+                    let dw = tern(n, 3);
+                    grid.contains(&(x + dx, y + dy, z + dz, w + dw))
+                }).count();
+                Some((x, y, z, w)).filter(|_|
+                    (grid.contains(&(x, y, z, w)) && count == 2)
+                    || (count == 3))
+                }).collect();
     }
     println!("{}", grid.len());
 }
