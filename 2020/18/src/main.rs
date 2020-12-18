311cbe1638e02fe0aecdf918953512d299bb55da
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-18 08:08:53 -0500

    2020, day 18

---
+++ 2020/18/src/main.rs
@@ -0,0 +1,90 @@
+use std::io::BufRead;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Token {
+    Int(i64),
+    ParenLeft,
+    ParenRight,
+    Add,
+    Mul,
+}
+
+fn next<I>(iter: &mut std::iter::Peekable<I>) -> Option<Token>
+    where I: Iterator<Item=char>
+{
+    // Eat all whitespace
+    while iter.peek() == Some(&' ') {
+        iter.next();
+    }
+
+    // First, tokenize any numbers
+    let mut num = None;
+    while let Some(c) = iter.peek() {
+        if c.is_numeric() {
+            num = Some(num.unwrap_or(0) * 10 + c.to_digit(10).unwrap() as i64);
+            iter.next();
+        } else {
+            break;
+        }
+    }
+    if let Some(n) = num {
+        return Some(Token::Int(n));
+    }
+
+    while let Some(c) = iter.next() {
+        match c {
+            '(' =>  return Some(Token::ParenLeft),
+            ')' =>  return Some(Token::ParenRight),
+            '+' =>  return Some(Token::Add),
+            '*' =>  return Some(Token::Mul),
+            _ => panic!("Invalid char: {}", c),
+        }
+    }
+    None
+}
+
+fn tokenize<'a>(s: &'a str) -> impl Iterator<Item=Token> + 'a {
+    let mut iter = s.chars().peekable();
+    std::iter::from_fn(move || next(&mut iter))
+}
+
+fn eval<I: Iterator<Item=Token>>(iter: &mut I) -> i64 {
+    let mut acc = None;
+    let mut op = None;
+
+    while let Some(token) = iter.next() {
+        println!("Got token {:?}, acc {:?}, op {:?}", token, acc, op);
+        let num = match token {
+            Token::ParenLeft => Some(eval(iter)),
+            Token::ParenRight => return acc.unwrap(),
+            Token::Add | Token::Mul => {
+                assert!(op == None);
+                op = Some(token);
+                None
+            },
+            Token::Int(i) => Some(i),
+        };
+        if let Some(n) = num {
+            if let Some(a) = acc {
+                match op.unwrap() {
+                    Token::Add => acc = Some(a + n),
+                    Token::Mul => acc = Some(a * n),
+                    _ => panic!("Invalid op {:?}", op),
+                }
+                op = None;
+            } else {
+                assert!(op == None);
+                acc = Some(n);
+            }
+        }
+    }
+    println!("Returning {:?}", acc);
+    acc.unwrap()
+}
+
+fn main() {
+    let out: i64 = std::io::stdin().lock().lines()
+        .map(|line| eval(&mut tokenize(&line.unwrap())))
+        .sum();
+    println!("Part 1: {}", out);
+}
