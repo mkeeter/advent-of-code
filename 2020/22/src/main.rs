21b27afb17d0984d1422bf773221f23a37b34b86
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-22 08:48:31 -0500

    Refactor playing into generic function

--- 2020/22/src/main.rs
+++ 2020/22/src/main.rs
@@ -6,30 +6,28 @@ use std::cmp::Ordering;
 enum Winner { PlayerA, PlayerB }
 type Deck = VecDeque<u8>;
 
-fn play_recursive(mut deck_a: Deck, mut deck_b: Deck) -> (Winner, Deck) {
+fn score(deck: Deck) -> u32 {
+    deck.into_iter()
+        .rev()
+        .zip(1..)
+        .map(|(i, c)| i as u32 * c)
+        .sum::<u32>()
+}
+
+fn play<F>(mut deck_a: Deck, mut deck_b: Deck, check: F) -> (Winner, u32)
+    where F: Fn(u8, u8, &Deck, &Deck) -> Winner
+{
     let mut seen: HashSet<(Deck, Deck)> = HashSet::new();
 
     while !deck_a.is_empty() && !deck_b.is_empty() {
-
+        // Break the infinite loop
         if !seen.insert((deck_a.clone(), deck_b.clone())) {
-            return (Winner::PlayerA, deck_a);
+            return (Winner::PlayerA, score(deck_a));
         }
 
         let a = deck_a.pop_front().unwrap();
         let b = deck_b.pop_front().unwrap();
-
-        let won = if deck_a.len() >= a as usize && deck_b.len() >= b as usize {
-            let deck_a = deck_a.iter().take(a as usize).copied().collect();
-            let deck_b = deck_b.iter().take(b as usize).copied().collect();
-            play_recursive(deck_a, deck_b).0
-        } else {
-            match a.cmp(&b) {
-                Ordering::Less => Winner::PlayerB,
-                Ordering::Greater => Winner::PlayerA,
-                _ => panic!("Invalid comparison; cards must be unique"),
-            }
-        };
-        match won {
+        match check(a, b, &deck_a, &deck_b) {
             Winner::PlayerA => {
                 deck_a.push_back(a);
                 deck_a.push_back(b);
@@ -42,9 +40,27 @@ fn play_recursive(mut deck_a: Deck, mut deck_b: Deck) -> (Winner, Deck) {
     }
 
     if deck_b.is_empty() {
-        (Winner::PlayerA, deck_a)
+        (Winner::PlayerA, score(deck_a))
     } else {
-        (Winner::PlayerB, deck_b)
+        (Winner::PlayerB, score(deck_b))
+    }
+}
+
+fn check_normal(a: u8, b: u8, _deck_a: &Deck, _deck_b: &Deck) -> Winner {
+    match a.cmp(&b) {
+        Ordering::Less => Winner::PlayerB,
+        Ordering::Greater => Winner::PlayerA,
+        _ => panic!("Invalid comparison; cards must be unique"),
+    }
+}
+
+fn check_recursive(a: u8, b: u8, deck_a: &Deck, deck_b: &Deck) -> Winner {
+    if deck_a.len() >= a as usize && deck_b.len() >= b as usize {
+        let deck_a = deck_a.iter().take(a as usize).copied().collect();
+        let deck_b = deck_b.iter().take(b as usize).copied().collect();
+        play(deck_a, deck_b, check_recursive).0
+    } else {
+        check_normal(a, b, deck_a, deck_b)
     }
 }
 
@@ -59,38 +75,9 @@ fn main() {
             .collect::<Deck>())
         .collect::<Vec<_>>();
 
-    let mut deck_a = decks[0].clone();
-    let mut deck_b = decks[1].clone();
+    let p1 = play(decks[0].clone(), decks[1].clone(), check_normal).1;
+    println!("Part 1: {}", p1);
 
-    while !deck_a.is_empty() && !deck_b.is_empty() {
-        let a = deck_a.pop_front().unwrap();
-        let b = deck_b.pop_front().unwrap();
-        match a.cmp(&b) {
-            Ordering::Less => {
-                deck_b.push_back(b);
-                deck_b.push_back(a);
-            },
-            Ordering::Greater => {
-                deck_a.push_back(a);
-                deck_a.push_back(b);
-            },
-            _ => panic!("Invalid ordering, cards must be unique"),
-        }
-    }
-
-    let score = deck_a.iter().rev()
-        .chain(deck_b.iter().rev())
-        .zip(1..)
-        .map(|(i, c)| *i as u32 * c)
-        .sum::<u32>();
-    println!("Part 1: {}", score);
-
-    let deck_a = decks[0].clone();
-    let deck_b = decks[1].clone();
-    let winner = play_recursive(deck_a, deck_b);
-    let score = winner.1.iter().rev()
-        .zip(1..)
-        .map(|(i, c)| *i as u32 * c)
-        .sum::<u32>();
-    println!("Part 2: {}", score);
+    let p2 = play(decks[0].clone(), decks[1].clone(), check_recursive).1;
+    println!("Part 2: {}", p2);
 }
