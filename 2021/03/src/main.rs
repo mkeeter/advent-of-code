65d3f47491f83613485dd321417af44d02d2e746
Matt Keeter <matt.j.keeter@gmail.com>
2021-12-03 08:41:21 -0500

    Refactor 'most common bit'

--- 2021/03/src/main.rs
+++ 2021/03/src/main.rs
@@ -1,49 +1,41 @@
 use std::io::BufRead;
 
+/// Returns true if the most common digit at position `bit` is `1` or a tie.
+fn common(nums: &[i32], bit: usize) -> bool {
+    0 <= nums
+        .iter()
+        .fold(0, |sum, i| sum + (((i >> bit) & 1) * 2 - 1))
+}
+
 fn rating(mut nums: Vec<i32>, bits: usize, mode: bool) -> i32 {
     for bit in (0..bits).rev() {
-        if nums.len() == 1 {
-            break;
-        }
-        let score = nums
-            .iter()
-            .fold(0, |sum, i| sum + (((i >> bit) & 1) * 2 - 1));
-        let target = (mode ^ (score >= 0)) as i32;
+        let target = (mode ^ common(&nums, bit)) as i32;
         nums = nums
             .into_iter()
             .filter(|n| ((n >> bit) & 1) == target)
             .collect();
+        if nums.len() == 1 {
+            return nums[0];
+        }
     }
-    assert!(nums.len() == 1);
-    nums[0]
+    panic!("Failed to reduce nums");
 }
 
 fn main() {
-    let lines = std::io::stdin()
-        .lock()
+    let stdin = std::io::stdin();
+    let mut iter = stdin.lock()
         .lines()
         .map(|line| line.unwrap())
-        .collect::<Vec<String>>();
+        .peekable();
 
-    let bits = lines[0].len();
-    let nums = lines
-        .into_iter()
+    let bits = iter.peek().unwrap().len();
+    let nums = iter
         .map(|line| i32::from_str_radix(&line, 2).unwrap())
         .collect::<Vec<i32>>();
 
-    let count = nums
-        .iter()
-        .flat_map(|n| (0..bits).map(move |b| (b, ((n >> b) & 1) * 2 - 1)))
-        .scan([0; 32], |accum, (b, d)| {
-            accum[b] += d;
-            Some(*accum)
-        })
-        .last()
-        .unwrap();
-
     let gamma = (0..bits)
-        .map(|i| ((count[i] > 0) as u32) << i)
-        .fold(0, |a, b| a | b);
+        .filter(|b| common(&nums, *b))
+        .fold(0, |out, b| out | (1 << b));
     let epsilon = (!gamma) & ((1 << bits) - 1);
 
     println!("Part 1: {}", gamma * epsilon);
