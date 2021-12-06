96b79564cc2ed93d8dc4216c23e836247225f540
Matt Keeter <matt.j.keeter@gmail.com>
2021-12-06 08:13:27 -0500

    Completely different strategy for Part 2

--- 2021/06/src/main.rs
+++ 2021/06/src/main.rs
@@ -1,17 +1,26 @@
-/// Returns the population of fish spawned by a single starting fish whose
-/// internal timer hits 0 on day `fish`
-fn pop(time: usize, fish: usize) -> usize {
-    let children = (time.saturating_sub(fish) + 6) / 7;
-    1 + (0..children).map(|i| pop(time, fish + i*7 + 9)).sum::<usize>()
+fn sim(fish: &[usize], days: usize) -> usize {
+    let mut spawn = vec![0; days + 9];
+    for &f in fish {
+        spawn[f] += 1;
+    }
+    let mut pop = fish.len();
+    for day in 1..days {
+        pop += spawn[day];
+        spawn[day + 7] += spawn[day];
+        spawn[day + 9] += spawn[day];
+    }
+    pop
 }
 
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
-    let nums = input.trim()
+    let fish = input
+        .trim()
         .split(',')
         .map(|num| num.parse().unwrap())
         .collect::<Vec<usize>>();
 
-    println!("Part 1: {}", nums.iter().map(|&fish| pop(80, fish)).sum::<usize>());
+    println!("Part 1: {}", sim(&fish, 80));
+    println!("Part 2: {}", sim(&fish, 256));
 }
