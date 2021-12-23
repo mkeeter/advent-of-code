bcc0c0320957d527f7e2d6eee4b187e2433ceef4
Matt Keeter <matt.j.keeter@gmail.com>
2021-12-23 14:02:04 -0500

    Day 23, part 2!

--- 2021/23/src/main.rs
+++ 2021/23/src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 use std::io::BufRead;
 
-#[derive(Copy, Clone, Default, Eq, PartialEq, Hash)]
-struct Map {
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+struct Map<const DEPTH: usize> {
     slab: [u8; 11],
-    wells: [[u8; 2]; 4],
-    locked: [[bool; 2]; 4],
+    wells: [[u8; DEPTH]; 4],
+    locked: [[bool; DEPTH]; 4],
 }
 
 fn energy(b: u8) -> usize {
@@ -17,14 +17,18 @@ fn energy(b: u8) -> usize {
         _ => panic!("Invalid ambipod '{}'", b),
     }
 }
-impl Map {
-    fn check(&self) {
-        for (x, c) in self.slab.iter().enumerate() {
-            if *c != 0 {
-                assert!(!(x == 2 || x == 4 || x == 6 || x == 8));
-            }
+
+impl<const DEPTH: usize> Default for Map<DEPTH> {
+    fn default() -> Self {
+        Map {
+            slab: [0; 11],
+            wells: [[0; DEPTH]; 4],
+            locked: [[false; DEPTH]; 4],
         }
     }
+}
+
+impl<const DEPTH: usize> Map<DEPTH> {
     fn explore(&self, cache: &mut HashMap<Self, Option<usize>>) -> Option<usize> {
         if let Some(s) = cache.get(self) {
             return *s;
@@ -44,73 +48,72 @@ impl Map {
 
         // Moves from the slab into the well
         for (i, &c) in self.slab.iter().enumerate().filter(|(_, &c)| c != 0) {
-            let target_well = (c - b'A') as usize;
-            // Skip any moves that put us into an invalid target well
-            if self.wells[target_well][1] != 0 && self.wells[target_well][1] != c {
+            let w = (c - b'A') as usize;
+
+            // Skip any wells which have non-final characters
+            if self.wells[w].iter().any(|&d| d != 0 && d != c) {
                 continue;
             }
+
             // Check whether the path is clear
-            let target_well_x = target_well * 2 + 2;
-            if (i.min(target_well_x)..=i.max(target_well_x)).any(|x| x != i && self.slab[x] != 0) {
+            let well_x = w * 2 + 2;
+            if (i.min(well_x)..=i.max(well_x)).any(|x| x != i && self.slab[x] != 0) {
                 continue;
             }
-            let well_bottom = if self.wells[target_well][1] == 0 {
-                1
-            } else {
-                0
-            };
+
+            let y =
+                if let Some(w) = (0..DEPTH).rev().find(|y| self.wells[w][*y] == 0) {
+                    w
+                } else {
+                    continue;
+                };
+
             let mut next = *self;
             next.slab[i] = 0;
-            next.wells[target_well][well_bottom] = c;
-            next.check();
-            next.locked[target_well][well_bottom] = true;
+            next.wells[w][y] = c;
+            next.locked[w][y] = true;
 
-            let dx = i.max(target_well_x) - i.min(target_well_x);
-            let dy = well_bottom + 1;
+            let dx = i.max(well_x) - i.min(well_x);
+            let dy = y + 1;
 
             todo.push((next, energy(c) * (dx + dy)));
         }
 
         // Moves from the wells into the slab
         for (w, well) in self.wells.iter().enumerate() {
-            let y = if well[0] != 0 {
-                0
-            } else if well[1] != 0 {
-                1
+            let y = if let Some(w) = (0..DEPTH).find(|y| well[*y] != 0) {
+                w
             } else {
                 continue;
             };
             if self.locked[w][y] {
                 continue;
             }
-            let c = well[y];
             let well_x = w * 2 + 2;
-            let mut x_targets = vec![];
+            let c = well[y];
+            let mut enqueue = |x| {
+                // Skip areas directly above wells
+                if x != 2 && x != 4 && x != 6 && x != 8 {
+                    let mut next = *self;
+                    next.wells[w][y] = 0;
+                    next.slab[x] = c;
+
+                    let dx = well_x.max(x) - well_x.min(x);
+                    let dy = y + 1;
+                    todo.push((next, energy(c) * (dx + dy)));
+                }
+            };
             for x in (0..well_x).rev() {
                 if self.slab[x] != 0 {
                     break;
                 }
-                x_targets.push(x);
+                enqueue(x);
             }
             for x in well_x..11 {
                 if self.slab[x] != 0 {
                     break;
                 }
-                x_targets.push(x);
-            }
-            for x in x_targets.into_iter() {
-                // Skip areas directly above wells
-                if x == 2 || x == 4 || x == 6 || x == 8 {
-                    continue;
-                }
-                let mut next = *self;
-                next.wells[w][y] = 0;
-                next.check();
-                next.slab[x] = c;
-
-                let dx = well_x.max(x) - well_x.min(x);
-                let dy = y + 1;
-                todo.push((next, energy(c) * (dx + dy)));
+                enqueue(x);
             }
         }
         let best = todo
@@ -121,7 +124,8 @@ impl Map {
         best
     }
 }
-impl std::fmt::Debug for Map {
+
+impl<const DEPTH: usize> std::fmt::Debug for Map<DEPTH> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         writeln!(f, "#############")?;
         write!(f, "#")?;
@@ -141,25 +145,28 @@ impl std::fmt::Debug for Map {
             } else {
                 write!(f, "{}#", well[0] as char)?;
             }
+        }
         writeln!(f, "##")?;
-        write!(f, "  #")?;
-        for (w, well) in self.wells.iter().enumerate() {
-            if well[1] == 0 {
-                write!(f, ".#")?;
-            } else if !self.locked[w][1] {
-                write!(f, "{}#", (well[1] + b'a' - b'A') as char)?;
-            } else {
-                write!(f, "{}#", well[1] as char)?;
+        for y in 1..DEPTH {
+            write!(f, "  #")?;
+            for (w, well) in self.wells.iter().enumerate() {
+                if well[y] == 0 {
+                    write!(f, ".#")?;
+                } else if !self.locked[w][y] {
+                    write!(f, "{}#", (well[y] + b'a' - b'A') as char)?;
+                } else {
+                    write!(f, "{}#", well[y] as char)?;
+                }
             }
+            writeln!(f)?;
         }
-        }
-        write!(f, "\n  #########")?;
+        write!(f, "  #########")?;
         Ok(())
     }
 }
 
 fn main() {
-    let mut map: Map = Map::default();
+    let mut map: Map<2> = Map::default();
     for (y, line) in std::io::stdin()
         .lock()
         .lines()
@@ -174,13 +181,16 @@ fn main() {
             }
         }
     }
-    println!("{:?}", map);
     let mut cache = HashMap::new();
-    let res = map.explore(&mut cache);
-    println!("{:?}", res);
-    /*
-    for m in res.1 {
-        println!("{:?}", m);
+    println!("Part 1: {}", map.explore(&mut cache).unwrap());
+
+    let mut map2: Map<4> = Map::default();
+    for (w, well) in map.wells.iter().enumerate() {
+        map2.wells[w][0] = well[0];
+        map2.wells[w][1] = "DCBA".as_bytes()[w];
+        map2.wells[w][2] = "DBAC".as_bytes()[w];
+        map2.wells[w][3] = well[1];
     }
-    */
+    let mut cache = HashMap::new();
+    println!("Part 2: {}", map2.explore(&mut cache).unwrap());
 }
