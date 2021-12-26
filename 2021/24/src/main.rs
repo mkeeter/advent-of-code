74c9eaa0d35bd10565e01d609d1451ee5fa35848
Matt Keeter <matt.j.keeter@gmail.com>
2021-12-26 16:53:45 -0500

    Add helper struct

--- 2021/24/src/main.rs
+++ 2021/24/src/main.rs
@@ -3,6 +3,23 @@ use std::io::{BufRead, Write};
 
 type State = ([i64; 4], (usize, usize));
 
+struct StateVec(Vec<State>);
+impl StateVec {
+    fn apply<'a, F, I>(&mut self, mut words: I, f: F)
+    where
+        F: Fn(i64, i64) -> i64 + std::marker::Send + std::marker::Sync,
+        I: Iterator<Item = &'a str>,
+    {
+        let ra = reg_index(words.next().unwrap());
+        let rb = words.next().unwrap();
+        self.0.par_iter_mut().for_each(|(regs, _)| {
+            let a = regs[ra];
+            let b = reg_value(rb, regs);
+            regs[ra] = f(a, b)
+        });
+    }
+}
+
 fn reg_index(s: &str) -> usize {
     match s {
         "x" => 0,
@@ -21,7 +38,7 @@ fn reg_value(s: &str, regs: &[i64]) -> i64 {
 }
 
 fn main() {
-    let mut state: Vec<State> = vec![([0; 4], (0, 0))];
+    let mut state = StateVec(vec![([0; 4], (0, 0))]);
 
     let lines = std::io::stdin()
         .lock()
@@ -35,91 +52,58 @@ fn main() {
             i + 1,
             lines.len(),
             line,
-            state.len()
+            state.0.len()
         );
         std::io::stdout().flush().unwrap();
 
         match words.next().unwrap() {
             "inp" => {
                 let r = reg_index(words.next().unwrap());
-                state.par_sort_unstable_by_key(|k| {
-                    let mut out = k.0;
-                    out[r] = 0;
-                    out
-                });
-                state = (1..=9)
+
+                // Clear the register that's about to be written
+                state.0.par_iter_mut().for_each(|k| k.0[r] = 0);
+
+                // Sort by register state, then do single-pass compaction
+                state.0.par_sort_unstable_by_key(|k| k.0);
+                let mut i = 0;
+                let mut j = 1;
+                while j < state.0.len() {
+                    if state.0[i].0 == state.0[j].0 {
+                        let (imin, imax) = state.0[i].1;
+                        let (jmin, jmax) = state.0[j].1;
+                        state.0[i].1 = (imin.min(jmin), imax.max(jmax));
+                    } else {
+                        i += 1;
+                        state.0[i] = state.0[j];
+                    }
+                    j += 1;
+                }
+                assert!(i < state.0.len());
+                state.0.resize(i + 1, ([0; 4], (0, 0)));
+
+                state.0 = (1..=9)
                     .into_par_iter()
-                    .map(|i| {
-                        let mut next: Vec<State> = Vec::with_capacity(state.len());
-                        for (mut regs, (min, max)) in state.iter() {
+                    .flat_map(|i| {
+                        state.0.par_iter().map(move |(mut regs, (min, max))| {
                             regs[r] = i;
-                            let min = min * 10 + i as usize;
-                            let max = max * 10 + i as usize;
-                            if next.last().map(|p| p.0 == regs).unwrap_or(false) {
-                                let (prev_min, prev_max) = &mut next.last_mut().unwrap().1;
-                                *prev_min = (*prev_min).min(min);
-                                *prev_max = (*prev_max).max(max);
-                            } else {
-                                next.push((regs, (min, max)));
-                            }
-                        }
-                        next
+                            (regs, (min * 10 + i as usize, max * 10 + i as usize))
+                        })
                     })
-                    .flatten()
                     .collect();
             }
-            "add" => {
-                let ra = reg_index(words.next().unwrap());
-                let rb = words.next().unwrap();
-                state.par_iter_mut().for_each(|(regs, _)| {
-                    let a = regs[ra];
-                    let b = reg_value(rb, regs);
-                    regs[ra] = a + b;
-                });
-            }
-            "mul" => {
-                let ra = reg_index(words.next().unwrap());
-                let rb = words.next().unwrap();
-                state.par_iter_mut().for_each(|(regs, _)| {
-                    let a = regs[ra];
-                    let b = reg_value(rb, regs);
-                    regs[ra] = a * b;
-                });
-            }
-            "div" => {
-                let ra = reg_index(words.next().unwrap());
-                let rb = words.next().unwrap();
-                state.par_iter_mut().for_each(|(regs, _)| {
-                    let a = regs[ra];
-                    let b = reg_value(rb, regs);
-                    regs[ra] = a / b;
-                });
-            }
-            "mod" => {
-                let ra = reg_index(words.next().unwrap());
-                let rb = words.next().unwrap();
-                state.par_iter_mut().for_each(|(regs, _)| {
-                    let a = regs[ra];
-                    let b = reg_value(rb, regs);
-                    regs[ra] = a % b;
-                });
-            }
-            "eql" => {
-                let ra = reg_index(words.next().unwrap());
-                let rb = words.next().unwrap();
-                state.par_iter_mut().for_each(|(regs, _)| {
-                    let a = regs[ra];
-                    let b = reg_value(rb, regs);
-                    regs[ra] = (a == b) as i64;
-                });
-            }
+            "add" => state.apply(words, |a, b| a + b),
+            "mul" => state.apply(words, |a, b| a * b),
+            "div" => state.apply(words, |a, b| a / b),
+            "mod" => state.apply(words, |a, b| a % b),
+            "eql" => state.apply(words, |a, b| (a == b) as i64),
             _ => panic!("Invalid instruction {}", line),
         }
     }
     println!(
         "Part 1: {}                         ",
         state
-            .iter()
+            .0
+            .par_iter()
             .filter(|(k, _)| k[2] == 0)
             .map(|(_, v)| v.1)
             .max()
@@ -128,7 +112,8 @@ fn main() {
     println!(
         "Part 2: {}                         ",
         state
-            .iter()
+            .0
+            .par_iter()
             .filter(|(k, _)| k[2] == 0)
             .map(|(_, v)| v.0)
             .min()
