a4ea1e6479541547b2ccd0b94e79c9be6af37cab
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-01 08:15:22 -0500

    Tweaks to download script

--- 2022/download.sh
+++ 2022/download.sh
@@ -6,9 +6,10 @@ DIR=$(basename "$PWD")
 YEAR="${YEAR:-$DIR}"
 DAY="${DAY:-$(date +"%d")}"
 
+echo " 📅  Preparing $YEAR-$DAY"
 if [ ! -d "$DAY/src" ]
 then
-    echo "✨ Making directory for day $DAY"
+    echo " ✨ Making directory for day $DAY"
     mkdir -p "$DAY/src"
     sed "s/template/day$DAY/g" template/Cargo.toml > "$DAY/Cargo.toml"
     cp template/src/main.rs "$DAY/src/"
@@ -22,11 +23,14 @@ then
     if [ $TODAY -ge $TARGET ]
     then
         COOKIE=$(cat ~/.aoc-cookie)
-        echo "⬇️  Downloading input for day $DAY"
-        curl "https://adventofcode.com/$YEAR/day/$DAY/input" -H "Cookie: $COOKIE" > "$DAY/input"
+        DAY_SHORT=$(echo $DAY | sed "s/^0*//g")
+        echo " ⬇️   Downloading input for day $DAY_SHORT"
+        curl -s "https://adventofcode.com/$YEAR/day/$DAY_SHORT/input" --cookie "session=$COOKIE" > "$DAY/input"
     else
-        echo "⚠️  $YEAR-$DAY is not yet available; skipping download"
+        echo " ⚠️   $YEAR-$DAY is not yet available; skipping download"
     fi
+else
+    echo " 🔎  $DAY/input already exists; skipping download"
 fi
 
 # Sanity-checking on the input
@@ -35,9 +39,11 @@ then
     if grep "Please don't repeatedly request this endpoint" "$DAY/input" > /dev/null
     then
         # This shouldn't happen, due to the date check above
-        echo "❌ Download failed due to rate-limiting on the server"
+        echo " ❌  Download failed due to rate-limiting on the server"
     elif grep "Puzzle inputs differ by user" "$DAY/input" > /dev/null
     then
-        echo "❌ Login failed; perhaps your cookie is stale?"
+        echo " ❌  Login failed; perhaps your cookie is stale?"
+    else
+        echo " ✅  Ready to go!"
     fi
 fi
