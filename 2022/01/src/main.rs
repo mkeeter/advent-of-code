b770bf19db44c880919bb7d5306dcf808065d409
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-01 08:30:43 -0500

    Use sort_unstable as a tiny optimization

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<()> {
         .split(String::is_empty)
         .map(|lines| lines.iter().map(|s| s.parse::<u64>()).sum())
         .collect::<Result<Vec<_>, _>>()?;
-    elves.sort();
+    elves.sort_unstable();
 
     println!("Part 1: {}", elves.last().unwrap());
     println!("Part 2: {}", elves.iter().rev().take(3).sum::<u64>());
