554ffb32e12ec123e11b370d684d67a3c20eec42
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-01 08:01:19 -0500

    2022, day 1

---
+++ 2022/01/src/main.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use std::io::BufRead;
+
+fn main() -> Result<()> {
+    let nums = std::io::stdin()
+        .lock()
+        .lines()
+        .collect::<Result<Vec<String>, _>>()?;
+
+    let mut elves = nums
+        .split(String::is_empty)
+        .map(|lines| lines.iter().map(|s| s.parse::<u64>()).sum())
+        .collect::<Result<Vec<_>, _>>()?;
+    elves.sort();
+
+    println!("Part 1: {}", elves.last().unwrap());
+    println!("Part 2: {}", elves.iter().rev().take(3).sum::<u64>());
+
+    Ok(())
+}
