a6f240be91e1d193672b007f599bf1745d2629cc
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-03 09:30:38 -0500

    More generic implementation

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -2,55 +2,49 @@ use anyhow::{anyhow, bail, Result};
 use std::collections::BTreeSet;
 use std::io::BufRead;
 
-fn score(c: char) -> Result<usize> {
-    match c {
-        'a'..='z' => Ok((c as u32 - 'a' as u32) as usize + 1),
-        'A'..='Z' => Ok((c as u32 - 'A' as u32) as usize + 27),
-        _ => bail!("Invalid character '{c}'"),
-    }
-}
-
-fn find_common_char(line: &[char]) -> Result<usize> {
-    let (a, b) = line.split_at(line.len() / 2);
-    let a: BTreeSet<char> = a.iter().cloned().collect();
-    let c = *b
-        .iter()
-        .find(|c| a.contains(c))
-        .ok_or_else(|| anyhow!("No common character"))?;
-    score(c)
+fn run<'a, I: Iterator<Item = &'a str>>(
+    iter: I,
+    group_size: usize,
+) -> Result<usize> {
+    let lines = iter
+        .map(|i| i.chars().collect::<BTreeSet<char>>())
+        .collect::<Vec<_>>();
+    lines
+        .chunks(group_size)
+        .map(|group| {
+            let common = group
+                .iter()
+                .cloned()
+                .reduce(|a, b| a.intersection(&b).cloned().collect())
+                .ok_or_else(|| anyhow!("Fold failed; empty input?"))?;
+            if common.len() != 1 {
+                bail!("Too many common items: {common:?}");
+            }
+            let c = common.into_iter().next().unwrap();
+            match c {
+                'a'..='z' => Ok((c as u32 - 'a' as u32) as usize + 1),
+                'A'..='Z' => Ok((c as u32 - 'A' as u32) as usize + 27),
+                _ => Err(anyhow!("Invalid character '{c}'")),
+            }
+        })
+        .sum()
 }
 
 fn main() -> Result<()> {
     let lines = std::io::stdin()
         .lock()
         .lines()
-        .map(|line| line.unwrap().chars().collect())
-        .collect::<Vec<Vec<char>>>();
+        .map(Result::unwrap)
+        .collect::<Vec<String>>();
 
-    let out = lines
-        .iter()
-        .map(|line| find_common_char(line))
-        .sum::<Result<usize, _>>()?;
+    let compartments = lines.iter().flat_map(|line| {
+        let (a, b) = line.split_at(line.len() / 2);
+        [a, b].into_iter()
+    });
+    let out = run(compartments, 2)?;
     println!("Part 1: {out}");
 
-    let out = lines
-        .chunks(3)
-        .map(|lines| {
-            let common = lines
-                .iter()
-                .cloned()
-                .reduce(|a, b| {
-                    let a: BTreeSet<char> = a.iter().cloned().collect();
-                    let b: BTreeSet<char> = b.iter().cloned().collect();
-                    a.intersection(&b).cloned().collect()
-                })
-                .ok_or_else(|| anyhow!("Fold failed; empty input?"))?;
-            if common.len() != 1 {
-                bail!("Too many common items: {common:?}");
-            }
-            score(common[0])
-        })
-        .sum::<Result<usize, _>>()?;
+    let out = run(lines.iter().map(String::as_str), 3)?;
     println!("Part 2: {out}");
 
     Ok(())
