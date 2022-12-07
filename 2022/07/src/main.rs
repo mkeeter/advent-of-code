91f18433d97aaf2a8b6bb8fc00258f8772c8f674
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-07 08:18:54 -0500

    2022, day 7

--- 2022/07/src/main.rs
+++ 2022/07/src/main.rs
@@ -67,21 +67,14 @@ impl Node {
         Ok(())
     }
 
-    fn count_sizes(&self) -> usize {
-        match self {
-            Node::File(..) => 0,
-            Node::Folder(b) => {
-                let size = self.size();
-                b.values().map(Node::count_sizes).sum::<usize>()
-                    + if size <= 100000 { size } else { 0 }
-            }
-        }
-    }
-
-    fn size(&self) -> usize {
+    fn folder_sizes(&self, out: &mut Vec<usize>) -> usize {
         match self {
             Node::File(s) => *s,
-            Node::Folder(b) => b.values().map(Node::size).sum(),
+            Node::Folder(b) => {
+                let size = b.values().map(|v| v.folder_sizes(out)).sum();
+                out.push(size);
+                size
+            }
         }
     }
 }
@@ -97,6 +90,28 @@ fn main() -> Result<()> {
     let mut filesystem = Node::Folder(BTreeMap::new());
     filesystem.run(&mut iter)?;
 
-    println!("Part 1: {}", filesystem.count_sizes());
+    let mut folder_sizes = vec![];
+    let root_size = filesystem.folder_sizes(&mut folder_sizes);
+
+    println!(
+        "Part 1: {}",
+        folder_sizes
+            .iter()
+            .filter(|i| **i <= 100_000)
+            .sum::<usize>()
+    );
+
+    const DISK_SPACE: usize = 70000000;
+    const REQUIRED_SPACE: usize = 30000000;
+    let unused = DISK_SPACE - root_size;
+    let min_free = REQUIRED_SPACE - unused;
+    println!(
+        "Part 2: {}",
+        folder_sizes
+            .iter()
+            .filter(|i| **i >= min_free)
+            .min()
+            .ok_or_else(|| anyhow!("No folders meeting min_free"))?
+    );
     Ok(())
 }
