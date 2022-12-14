4225ab663df2905ad30a3f54227a7d20a224a8f1
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-14 08:05:15 -0500

    2022, day 14 (actually committed)

--- 2022/14/src/main.rs
+++ 2022/14/src/main.rs
@@ -17,6 +17,43 @@ enum Tile {
     Sand,
 }
 
+fn run(map: &BTreeMap<Pos, Tile>, floor: bool) -> usize {
+    let y_max = map.keys().map(|p| p.y).max().unwrap_or(0);
+    let mut map = map.clone();
+    const SOURCE: Pos = Pos { x: 500, y: 0 };
+    'outer: while !map.contains_key(&SOURCE) {
+        let mut pos = Pos { x: 500, y: 0 };
+        loop {
+            let y = pos.y + 1;
+            let below = Pos { x: pos.x, y };
+            if below.y == y_max + 2 {
+                if floor {
+                    map.insert(pos, Tile::Sand);
+                    break;
+                } else {
+                    break 'outer;
+                }
+            } else if !map.contains_key(&below) {
+                pos = below;
+            } else {
+                let left = Pos { x: pos.x - 1, y };
+                if !map.contains_key(&left) {
+                    pos = left;
+                } else {
+                    let right = Pos { x: pos.x + 1, y };
+                    if !map.contains_key(&right) {
+                        pos = right;
+                    } else {
+                        map.insert(pos, Tile::Sand);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    map.values().filter(|v| **v == Tile::Sand).count()
+}
+
 fn main() -> Result<()> {
     let mut map = BTreeMap::new();
     for line in std::io::stdin().lock().lines() {
@@ -36,74 +73,8 @@ fn main() -> Result<()> {
         }
     }
 
-    let y_max = map.keys().map(|p| p.y).max().unwrap_or(0);
-    const SOURCE: Pos = Pos { x: 500, y: 0 };
+    println!("Part 1: {}", run(&map, false));
+    println!("Part 2: {}", run(&map, true));
 
-    {
-        let mut map = map.clone();
-        'outer: loop {
-            let mut pos = SOURCE;
-            loop {
-                let y = pos.y + 1;
-                if pos.y > y_max {
-                    break 'outer;
-                }
-                let below = Pos { x: pos.x, y };
-                if !map.contains_key(&below) {
-                    pos = below;
-                } else {
-                    let left = Pos { x: pos.x - 1, y };
-                    if !map.contains_key(&left) {
-                        pos = left;
-                    } else {
-                        let right = Pos { x: pos.x + 1, y };
-                        if !map.contains_key(&right) {
-                            pos = right;
-                        } else {
-                            println!("Sand settled at {},{}", pos.x, pos.y);
-                            map.insert(pos, Tile::Sand);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        println!(
-            "Part 1: {}",
-            map.values().filter(|v| **v == Tile::Sand).count()
-        );
-    }
-
-    while !map.contains_key(&SOURCE) {
-        let mut pos = Pos { x: 500, y: 0 };
-        loop {
-            let y = pos.y + 1;
-            let below = Pos { x: pos.x, y };
-            if below.y == y_max + 2 {
-                map.insert(pos, Tile::Sand);
-                break;
-            } else if !map.contains_key(&below) {
-                pos = below;
-            } else {
-                let left = Pos { x: pos.x - 1, y };
-                if !map.contains_key(&left) {
-                    pos = left;
-                } else {
-                    let right = Pos { x: pos.x + 1, y };
-                    if !map.contains_key(&right) {
-                        pos = right;
-                    } else {
-                        map.insert(pos, Tile::Sand);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    println!(
-        "Part 2: {}",
-        map.values().filter(|v| **v == Tile::Sand).count()
-    );
     Ok(())
 }
