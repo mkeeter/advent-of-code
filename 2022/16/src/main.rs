e3b1680a6a0ec942943ddbf9fb3b6e35fc6f563f
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-16 21:36:58 -0500

    Dramatic refactoring for a significant speedup

--- 2022/16/src/main.rs
+++ 2022/16/src/main.rs
@@ -1,32 +1,205 @@
 use anyhow::{anyhow, bail, Error, Result};
 use bimap::BiBTreeMap;
-use parse_display::{Display, FromStr};
+use parse_display::FromStr;
 use std::{
-    collections::{BTreeMap, VecDeque},
-    io::BufRead,
+    collections::{btree_map::Entry, BTreeMap},
+    io::{BufRead, Write},
 };
 
-#[derive(Clone, Debug, FromStr, Display)]
-#[display("Valve {name} has flow rate={rate}; {_t} {_l} to {_v} {out}")]
+////////////////////////////////////////////////////////////////////////////////
+
+#[derive(Clone, Debug, FromStr)]
+#[from_str(regex = "Valve (?P<name>[A-Z]+) has flow rate=(?P<rate>[0-9]+); \
+                    tunnels? leads? to valves? (?P<out>.*)")]
 struct Input {
     name: String,
     rate: u64,
     out: String,
-    _t: String,
-    _l: String,
-    _v: String,
 }
 
+/// Single room in the map, with outlets as indexes into a `Vec<Room>`
 struct Room {
     flow_rate: u64,
     out: Vec<usize>,
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
-struct State {
-    my_room: usize,
-    el_room: usize,
+struct State<const N: usize> {
+    people: [usize; N],
     valves: u64,
+}
+
+impl<const N: usize> State<N> {
+    fn canonicalize(&mut self) {
+        self.people.sort()
+    }
+}
+
+enum Action {
+    Open(usize),
+    MoveTo(usize),
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+fn run<const N: usize>(
+    rooms: &[Room],
+    start_time: usize,
+    end_time: usize,
+    start_room: usize,
+) -> u64 {
+    let mut todo = vec![];
+    todo.push((
+        State {
+            people: [start_room; N],
+            valves: 0,
+        },
+        0,
+    ));
+
+    let flow_rate = |state: &State<N>| -> u64 {
+        rooms
+            .iter()
+            .enumerate()
+            .map(|(i, room)| {
+                if state.valves & (1 << i) != 0 {
+                    room.flow_rate
+                } else {
+                    0
+                }
+            })
+            .sum()
+    };
+
+    // Breadth-first search
+    let mut seen = BTreeMap::new();
+    for time in start_time..end_time {
+        if time != start_time {
+            print!("\r");
+        }
+        print!("{time} / 30  ");
+        std::io::stdout().lock().flush().unwrap();
+
+        // Elephants and humans are interchangeable
+        todo.iter_mut().for_each(|(state, _)| state.canonicalize());
+
+        // Deduplicate any identical states that have released strictly less
+        // pressure than the best case for this state.
+        todo.sort_by_key(|(state, n)| (*state, std::cmp::Reverse(*n)));
+        todo.dedup_by_key(|(state, _)| *state);
+
+        // Release any state which has already been seen with a better score
+        todo.retain(|(state, released)| match seen.entry(*state) {
+            Entry::Vacant(v) => {
+                v.insert(*released);
+                true
+            }
+            Entry::Occupied(mut o) => {
+                if o.get() >= released {
+                    false
+                } else {
+                    o.insert(*released);
+                    true
+                }
+            }
+        });
+
+        // Sort by the number of set bits on a per-room basis, so we can
+        // deduplicate further.
+        todo.sort_by_key(|(s, _)| (s.people, s.valves.count_ones()));
+
+        let mut next = vec![];
+        for (i, (s, rs)) in todo.iter().enumerate() {
+            let mut j = i + 1;
+            let mut good = true;
+            while let Some((t, rt)) = todo.get(j) {
+                if s.people != t.people {
+                    break;
+                }
+                if s.valves & t.valves == s.valves && rt >= rs {
+                    good = false;
+                    break;
+                }
+                j += 1;
+            }
+            if good {
+                next.push((*s, *rs));
+            }
+        }
+        todo = std::mem::take(&mut next);
+
+        // Figure out the lower bound on best score (which is "stand still and
+        // continue to accumulate"), then filter any positions that couldn't
+        // possibly hit it.
+        let minimum_best_score = todo
+            .iter()
+            .map(|(s, rs)| {
+                let flow_rate = flow_rate(s);
+                rs + flow_rate * (end_time - time) as u64
+            })
+            .max()
+            .unwrap_or(0);
+        todo.retain(|(s, mut r)| {
+            let mut todo: Vec<u64> = rooms
+                .iter()
+                .enumerate()
+                .filter(|(i, r)| s.valves & (1 << i) == 0 && r.flow_rate > 0)
+                .map(|(_i, r)| r.flow_rate)
+                .collect();
+            let mut flow_rate = flow_rate(s);
+            todo.sort_unstable();
+            for _t in time..end_time {
+                r += flow_rate;
+                for _ in 0..N {
+                    flow_rate += todo.pop().unwrap_or(0);
+                }
+            }
+            r >= minimum_best_score
+        });
+
+        // Okay, we've done enough filtering.  Time to run the next step of the
+        // simulation and build up `next`
+        for (state, mut released) in todo.into_iter() {
+            // Update pressure based on open valves
+            released += flow_rate(&state);
+
+            // Every person (elephants count as people) can take a few different
+            // actions to modify the current state.
+            let mut new_states = vec![state];
+            for (i, &r) in state.people.iter().enumerate() {
+                let mut actions = vec![];
+                // If we can open this valve, then do so!
+                if state.valves & (1 << r) == 0 && rooms[r].flow_rate > 0 {
+                    actions.push(Action::Open(r));
+                }
+                // Try moving to every room that's attached to this one.
+                for &o in &rooms[r].out {
+                    actions.push(Action::MoveTo(o));
+                }
+                // Apply these actions to our state and accumulate new
+                // states as we go.
+                let mut new_new_states = vec![];
+                for s in new_states.into_iter() {
+                    for a in actions.iter() {
+                        let mut s = s;
+                        match a {
+                            Action::Open(o) => s.valves |= 1 << o,
+                            Action::MoveTo(t) => s.people[i] = *t,
+                        }
+                        new_new_states.push(s);
+                    }
+                }
+                new_states = new_new_states;
+            }
+            next.extend(new_states.into_iter().map(|s| (s, released)));
+        }
+        todo = next;
+    }
+
+    print!("\r");
+    todo.iter().map(|i| i.1).max().unwrap_or(0)
 }
 
 fn main() -> Result<()> {
@@ -66,170 +239,12 @@ fn main() -> Result<()> {
         bail!("Cannot pack {} rooms into a bitmask", rooms.len());
     }
 
-    let all_on = rooms
-        .iter()
-        .enumerate()
-        .filter(|(_i, r)| r.flow_rate > 0)
-        .map(|(i, _r)| 1 << i)
-        .reduce(|a, b| a | b)
-        .ok_or_else(|| anyhow!("Empty input?"))?;
-
-    let mut todo = vec![];
     let start_room = *name_to_index
         .get_by_right("AA")
         .ok_or_else(|| anyhow!("Could not find 'AA'"))?;
-    todo.push((
-        State {
-            my_room: start_room,
-            el_room: start_room,
-            valves: 0,
-        },
-        0,
-    ));
 
-    // Breadth-first search
-    let mut seen = BTreeMap::new();
-    for _t in 0..30 {
-        let mut next = vec![];
-        while let Some((state, mut released)) = todo.pop() {
-            if let Some(e) = seen.get_mut(&state) {
-                if *e >= released {
-                    continue;
-                } else {
-                    *e = released;
-                }
-            } else {
-                seen.insert(state, released);
-            }
-
-            // Update pressure based on open valves
-            for (i, r) in rooms.iter().enumerate() {
-                if state.valves & (1 << i) != 0 {
-                    released += r.flow_rate;
-                }
-            }
-
-            if state.valves == all_on {
-                next.push((state, released));
-                continue;
-            }
-
-            // If we can open this valve, then do so!
-            if state.valves & (1 << state.my_room) == 0
-                && rooms[state.my_room].flow_rate > 0
-            {
-                next.push((
-                    State {
-                        valves: state.valves | (1 << state.my_room),
-                        my_room: state.my_room,
-                        el_room: state.el_room,
-                    },
-                    released,
-                ));
-            }
-            for &o in &rooms[state.my_room].out {
-                next.push((
-                    State {
-                        my_room: o,
-                        el_room: state.el_room,
-                        valves: state.valves,
-                    },
-                    released,
-                ));
-            }
-        }
-        std::mem::swap(&mut todo, &mut next);
-    }
-    println!("Part 1: {}", todo.iter().map(|i| i.1).max().unwrap_or(0));
-
-    // Breadth-first search
-    let mut todo = vec![];
-    todo.push((
-        State {
-            my_room: start_room,
-            el_room: start_room,
-            valves: 0,
-        },
-        0,
-    ));
-    let mut seen = BTreeMap::new();
-    for _t in 4..30 {
-        println!("{_t}");
-        let mut next = vec![];
-        while let Some((state, mut released)) = todo.pop() {
-            let canonical_state = State {
-                my_room: state.my_room.min(state.el_room),
-                el_room: state.my_room.max(state.el_room),
-                valves: state.valves,
-            };
-            if let Some(e) = seen.get_mut(&canonical_state) {
-                if *e >= released {
-                    continue;
-                } else {
-                    *e = released;
-                }
-            } else {
-                seen.insert(canonical_state, released);
-            }
-
-            // Update pressure based on open valves
-            for (i, r) in rooms.iter().enumerate() {
-                if state.valves & (1 << i) != 0 {
-                    released += r.flow_rate;
-                }
-            }
-
-            if state.valves == all_on {
-                next.push((state, released));
-                continue;
-            }
-
-            struct Action {
-                open: u64,
-                move_to: usize,
-            }
-            let mut actions =
-                [state.my_room, state.el_room].into_iter().map(|room| {
-                    let mut out = vec![];
-                    // If we can open this valve, then do so!
-                    if state.valves & (1 << room) == 0
-                        && rooms[room].flow_rate > 0
-                    {
-                        out.push(Action {
-                            open: 1 << room,
-                            move_to: room,
-                        });
-                    }
-                    for &o in &rooms[room].out {
-                        out.push(Action {
-                            open: 0,
-                            move_to: o,
-                        });
-                    }
-                    out
-                });
-            let my_actions = actions.next().unwrap();
-            let el_actions = actions.next().unwrap();
-            for a in &my_actions {
-                for b in &el_actions {
-                    next.push((
-                        State {
-                            my_room: a.move_to,
-                            el_room: b.move_to,
-                            valves: state.valves | a.open | b.open,
-                        },
-                        released,
-                    ));
-                }
-            }
-        }
-        next.sort_by_key(|(state, released)| {
-            (*state, std::cmp::Reverse(*released))
-        });
-        next.dedup_by_key(|(state, _released)| *state);
-        std::mem::swap(&mut todo, &mut next);
-    }
-    println!("Part 2: {}", todo.iter().map(|i| i.1).max().unwrap_or(0));
+    println!("Part 1: {}", run::<1>(&rooms, 0, 30, start_room));
+    println!("Part 2: {}", run::<2>(&rooms, 4, 30, start_room));
 
     Ok(())
 }
