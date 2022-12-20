ec15f1dcb7891c2d7bc38ae087f8a7aac65a6922
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-20 09:22:33 -0500

    Got part 2!

--- 2022/20/src/main.rs
+++ 2022/20/src/main.rs
@@ -10,11 +10,16 @@ struct Node {
 
 fn mix(mut nodes: Vec<Node>) -> Vec<Node> {
     for i in 0..nodes.len() {
-        let v = nodes[i].value;
+        let v = nodes[i].value.rem_euclid((nodes.len() - 1) as i64);
 
         // The 0 node does not move
         if v == 0 {
             continue;
+        }
+
+        let mut j = i;
+        for _ in 0..v.abs() {
+            j = nodes[j].next;
         }
 
         // Cut node `i` out of the list
@@ -22,57 +27,31 @@ fn mix(mut nodes: Vec<Node>) -> Vec<Node> {
         let next = nodes[i].next;
         nodes[prev].next = next;
         nodes[next].prev = prev;
-        assert!(next != prev);
-
-        let mut j = i;
-        for _ in 0..v.abs() {
-            if v < 0 {
-                j = nodes[j].prev;
-            } else {
-                j = nodes[j].next;
-            }
-        }
-
-        // We are going to insert node `i` after node `j`, so take an extra step
-        // back if we're iterating backwards through the list.
-        if v < 0 {
-            j = nodes[j].prev;
-        }
 
         // Insert `i` after `j`
         let n = nodes[j].next;
-        assert!(i != j);
-        assert!(i != n);
-        assert!(j != n);
         nodes[j].next = i;
         nodes[n].prev = i;
         nodes[i].prev = j;
         nodes[i].next = n;
-
-        // Sanity checking of list invariants
-        for (i, n) in nodes.iter().enumerate() {
-            assert_eq!(i, nodes[n.prev].next);
-            assert_eq!(i, nodes[n.next].prev);
-        }
-        let mut j = i;
-        for _ in 0..nodes.len() {
-            j = nodes[j].next;
-        }
-        assert_eq!(j, i);
-
-        /*
-        let mut j = i;
-        loop {
-            print!("{} ", nodes[j].value);
-            j = nodes[j].next;
-            if j == i {
-                break;
-            }
-        }
-        println!();
-        */
     }
     nodes
+}
+
+fn score(nodes: &[Node]) -> i64 {
+    let (mut i, _) = nodes
+        .iter()
+        .enumerate()
+        .find(|(_, n)| n.value == 0)
+        .unwrap();
+    let mut sum = 0;
+    for _ in 0..3 {
+        for _ in 0..1000 {
+            i = nodes[i].next;
+        }
+        sum += nodes[i].value;
+    }
+    sum
 }
 
 fn main() -> Result<()> {
@@ -96,26 +75,21 @@ fn main() -> Result<()> {
     // Stitch together the list
     nodes[0].prev = nodes.len() - 1;
     nodes.last_mut().unwrap().next = 0;
-    for (i, n) in nodes.iter().enumerate() {
-        assert_eq!(i, nodes[n.prev].next);
-        assert_eq!(i, nodes[n.next].prev);
-    }
 
     let mixed = mix(nodes.clone());
-    let (mut i, _) = mixed
-        .iter()
-        .enumerate()
-        .find(|(_, n)| n.value == 0)
-        .unwrap();
-    let mut sum = 0;
-    for _ in 0..3 {
-        for _ in 0..1000 {
-            i = mixed[i].next;
-        }
-        println!("sum += {}", mixed[i].value);
-        sum += mixed[i].value;
+    println!("Part 1: {}", score(&mixed));
+
+    let mut mixed = nodes
+        .into_iter()
+        .map(|n| Node {
+            value: n.value * 811589153,
+            ..n
+        })
+        .collect::<Vec<Node>>();
+    for _ in 0..10 {
+        mixed = mix(mixed);
     }
-    println!("Part 1: {sum}");
-    // 17042 is too high
+    println!("Part 2: {}", score(&mixed));
+
     Ok(())
 }
