c18fedc9f18a08e373840c10c873b68acf0ecb05
Matt Keeter <matt.j.keeter@gmail.com>
2022-12-25 10:02:12 -0500

    Simpler encoding

--- 2022/25/src/main.rs
+++ 2022/25/src/main.rs
@@ -19,22 +19,14 @@ fn decode(s: &str) -> Result<i64> {
 
 fn encode(mut i: i64) -> String {
     let mut out = vec![];
-    let mut remainder = 0;
+    let mut remainder = false;
     while i != 0 {
-        let (c, new_remainder) = match (i % 5) + remainder {
-            0 => (b'0', 0),
-            1 => (b'1', 0),
-            2 => (b'2', 0),
-            3 => (b'=', 1),
-            4 => (b'-', 1),
-            5 => (b'0', 1),
-            _ => unreachable!(),
-        };
-        remainder = new_remainder;
-        out.push(c);
+        let v = (i % 5) + remainder as i64;
+        out.push([b'0', b'1', b'2', b'=', b'-'][(v % 5) as usize]);
+        remainder = v >= 3;
         i /= 5;
     }
-    if remainder != 0 {
+    if remainder {
         out.push(b'1');
     } else if out.is_empty() {
         out.push(b'0');
