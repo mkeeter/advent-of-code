59cc586cfe8ef47b6dd70c301ec3107287f43cba
Matt Keeter <matt.j.keeter@gmail.com>
2023-12-08 09:15:47 -0500

    Poking at more optimization

--- 2023/08/src/main.rs
+++ 2023/08/src/main.rs
@@ -1,7 +1,8 @@
-use anyhow::{bail, Result};
-use std::{collections::BTreeSet, io::BufRead, str::FromStr};
+use std::{io::BufRead, str::FromStr};
 
+use anyhow::{bail, Result};
 use parse_display::FromStr;
+use rayon::prelude::*;
 
 #[derive(FromStr, PartialEq)]
 #[display("{start} = ({left}, {right})")]
@@ -33,14 +34,20 @@ impl std::str::FromStr for Node {
         let mut out = 0u16;
         for c in s.chars() {
             let i = match c {
-                'A'..='Z' => c as u8 - b'A',
-                '0'..='9' => c as u8 - b'9',
+                'A'..='Z' => c as u16 - b'A' as u16,
+                '0'..='9' => c as u16 - b'9' as u16,
                 _ => bail!("Invalid character {c}"),
             };
-            out = out * 36 + i as u16;
+            out = out * 36 + i;
         }
         Ok(Self(out))
     }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Direction {
+    Left,
+    Right,
 }
 
 fn main() -> Result<()> {
@@ -49,71 +56,74 @@ fn main() -> Result<()> {
         .lines()
         .collect::<Result<Vec<String>, _>>()?;
 
-    let mut map = vec![];
+    let dirs = lines[0]
+        .chars()
+        .map(|c| match c {
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            c => panic!("invalid direction '{c}'"),
+        })
+        .collect::<Vec<_>>();
+
+    let mut map = vec![(Node(u16::MAX), Node(u16::MAX)); 36usize.pow(3)];
     let mut nodes = vec![];
     for line in &lines[2..] {
         let i = line.parse::<Input>().unwrap();
         let n = i.start.0 as usize;
-        if n >= map.len() {
-            map.resize(n + 1, (Node(u16::MAX), Node(u16::MAX)));
-        }
         map[n] = (i.left, i.right);
         nodes.push(i.start);
     }
 
-    let mut pos = Node::from_str("AAA").unwrap();
+    let run = |start: Node| {
+        std::iter::repeat(&dirs)
+            .flat_map(|c| c.iter())
+            .enumerate()
+            .scan(start, |pos, (i, lr)| {
+                match lr {
+                    Direction::Left => *pos = map[pos.0 as usize].0,
+                    Direction::Right => *pos = map[pos.0 as usize].1,
+                }
+                Some((i + 1, *pos))
+            })
+    };
+
     let dest = Node::from_str("ZZZ").unwrap();
-    for (i, lr) in std::iter::repeat(&lines[0])
-        .flat_map(|c| c.chars())
-        .enumerate()
-    {
+    for (i, pos) in run(Node::from_str("AAA").unwrap()) {
         if pos == dest {
             println!("Part 1: {i}");
             break;
         }
-        match lr {
-            'L' => pos = map[pos.0 as usize].0,
-            'R' => pos = map[pos.0 as usize].1,
-            c => panic!("invalid direction '{c}'"),
-        }
     }
 
-    let starts: Vec<_> = nodes.iter().filter(|n| n.is_start()).collect();
-    let mut periods = Vec::with_capacity(starts.len());
-    for start in starts {
-        let mut pos = *start;
-        let mut seen = None;
-        for (i, lr) in std::iter::repeat(&lines[0])
-            .flat_map(|c| c.chars())
-            .enumerate()
-        {
-            if pos.is_end() {
-                if let Some((prev_end, prev_steps)) = seen {
-                    assert_eq!(prev_end, pos, "multi-end loop detected");
-                    assert_eq!(
-                        prev_steps % lines[0].len(),
-                        i % lines[0].len(),
-                        "offset detected"
-                    );
-                    periods.push((prev_steps, i - prev_steps));
-                    break;
-                } else {
-                    seen = Some((pos, i));
+    let mut periods = nodes
+        .par_iter()
+        .filter(|n| n.is_start())
+        .map(|start| {
+            let mut seen = None;
+            for (i, pos) in run(*start) {
+                if pos.is_end() {
+                    if let Some((prev_end, prev_steps)) = seen {
+                        assert_eq!(prev_end, pos, "multi-end loop detected");
+                        assert_eq!(
+                            prev_steps % lines[0].len(),
+                            i % lines[0].len(),
+                            "offset detected"
+                        );
+                        assert_eq!(
+                            prev_steps,
+                            i - prev_steps,
+                            "oh no, the input isn't deliberately easy"
+                        );
+                        return prev_steps;
+                    } else {
+                        seen = Some((pos, i));
+                    }
                 }
             }
-            match lr {
-                'L' => pos = map[pos.0 as usize].0,
-                'R' => pos = map[pos.0 as usize].1,
-                c => panic!("invalid direction '{c}'"),
-            }
-        }
-    }
+            unreachable!()
+        })
+        .collect::<Vec<usize>>();
 
-    for (off, per) in &periods {
-        assert_eq!(off, per, "oh no, the input isn't deliberately easy");
-    }
-
-    let mut periods = periods.into_iter().map(|p| p.0).collect::<Vec<usize>>();
     while periods.len() > 1 {
         periods.sort();
         let mut a = periods[0];
