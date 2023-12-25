fc01e21a1877035167ddac4c39a15d1a717b7545
Matt Keeter <matt.j.keeter@gmail.com>
2023-12-25 12:04:14 -0500

    2023 done ⭐

--- 2023/25/src/lib.rs
+++ 2023/25/src/lib.rs
@@ -1,4 +1,105 @@
+use rand::Rng;
+use std::{
+    collections::BTreeMap,
+    sync::atomic::{AtomicBool, Ordering},
+    thread::available_parallelism,
+};
+
+#[derive(Clone, Default, Debug)]
+struct Graph<'a> {
+    nodes: BTreeMap<&'a str, usize>,
+    edges: BTreeMap<&'a str, BTreeMap<&'a str, usize>>,
+}
+
+impl<'a> Graph<'a> {
+    fn new() -> Self {
+        Self::default()
+    }
+    fn insert_edge(&mut self, src: &'a str, dst: &'a str) {
+        self.nodes.insert(src, 1);
+        self.nodes.insert(dst, 1);
+        self.edges.entry(src).or_default().insert(dst, 1);
+        self.edges.entry(dst).or_default().insert(src, 1);
+    }
+    fn random_edge(&self) -> (&'a str, &'a str) {
+        let num_edges = self.edges.values().map(|e| e.len()).sum();
+        let mut i = rand::thread_rng().gen_range(0..num_edges);
+        for (src, edges) in &self.edges {
+            if i < edges.len() {
+                let (dst, _) = edges.iter().nth(i).unwrap();
+                return (src, dst);
+            } else {
+                i -= edges.len();
+            }
+        }
+        unreachable!();
+    }
+    fn remove_edge(&mut self, edge: (&'a str, &'a str)) {
+        let (src, dst) = edge;
+        // We'll merge dst into src, since we have to pick a polarity
+        *self.nodes.get_mut(src).unwrap() += self.nodes.remove(dst).unwrap();
+        let e = self.edges.remove(dst).unwrap();
+        for (neighbor_name, n) in &e {
+            let neighbor = self.edges.get_mut(neighbor_name).unwrap();
+            let m = neighbor.remove(dst).unwrap();
+            assert_eq!(*n, m);
+            *neighbor.entry(src).or_default() += n;
+            *self
+                .edges
+                .get_mut(src)
+                .unwrap()
+                .entry(neighbor_name)
+                .or_default() += n;
+        }
+        self.edges.get_mut(src).unwrap().remove(src).unwrap();
+    }
+}
+
+fn search(g: &Graph, done: &AtomicBool) -> Option<usize> {
+    'outer: while !done.load(Ordering::Acquire) {
+        let mut g = g.clone();
+        while g.nodes.len() > 2 {
+            // Short-circuit if all cuts are > 3
+            if g.edges.values().all(|g| g.values().all(|e| *e > 3)) {
+                continue 'outer;
+            }
+            let e = g.random_edge();
+            g.remove_edge(e);
+        }
+        let mut iter = g.nodes.keys();
+        let src = iter.next().unwrap();
+        let dst = iter.next().unwrap();
+        if g.edges[src][dst] == 3 {
+            done.store(true, Ordering::Release);
+            return Some(g.nodes[src] * g.nodes[dst]);
+        }
+    }
+    None
+}
+
 pub fn solve(s: &str) -> (String, String) {
-    let _ = s;
-    ("unimplemented".to_owned(), "unimplemented".to_owned())
+    let mut graph = Graph::new();
+    for line in s.lines() {
+        let mut iter = line.split(": ");
+        let src = iter.next().unwrap();
+        for dst in iter.next().unwrap().split_whitespace() {
+            graph.insert_edge(src, dst);
+        }
+    }
+
+    let done = std::sync::atomic::AtomicBool::new(false);
+    let p1 = std::thread::scope(|s| {
+        let mut h = vec![];
+        for _ in 0..available_parallelism().unwrap().into() {
+            h.push(s.spawn(|| search(&graph, &done)));
+        }
+        let mut sol = None;
+        for h in h {
+            if let Some(v) = h.join().unwrap() {
+                sol = Some(v);
+            }
+        }
+        sol.unwrap()
+    });
+    (p1.to_string(), "⭐".to_owned())
 }
--- README.md
+++ README.md
@@ -1,24 +1,16 @@
-Solutions to [Advent of Code](https://adventofcode.com) for all years (2015-2022).
+Solutions to [Advent of Code](https://adventofcode.com) for all years (2015-2023).
 
-|🗓 Year                                |🌟 Stars |🔠 Languages        |✨ Standardized |
-|---------------------------------------|---------|---------------------|--------------|
-| [2015](https://adventofcode.com/2015) | 50      | Rust                | ✓            |
-| [2016](https://adventofcode.com/2016) | 50      | Rust                | ✓            |
-| [2017](https://adventofcode.com/2017) | 50      | IPython, C, Haskell | ✗            |
-| [2018](https://adventofcode.com/2018) | 50      | Rust                | ?            |
-| [2019](https://adventofcode.com/2019) | 50      | Rust                | ✓            |
-| [2020](https://adventofcode.com/2020) | 50      | Rust                | ✓            |
-| [2021](https://adventofcode.com/2021) | 50      | Rust                | ✓            |
-| [2022](https://adventofcode.com/2022) | 50      | Rust                | ✓            |
-| [2023](https://adventofcode.com/2023) | 48      | Rust                | ✓            |
+|🗓 Year                                |🌟 Stars |🔠 Languages        |
+|---------------------------------------|---------|---------------------|
+| [2015](https://adventofcode.com/2015) | 50      | Rust                |
+| [2016](https://adventofcode.com/2016) | 50      | Rust                |
+| [2017](https://adventofcode.com/2017) | 50      | IPython, C, Haskell |
+| [2018](https://adventofcode.com/2018) | 50      | Rust                |
+| [2019](https://adventofcode.com/2019) | 50      | Rust                |
+| [2020](https://adventofcode.com/2020) | 50      | Rust                |
+| [2021](https://adventofcode.com/2021) | 50      | Rust                |
+| [2022](https://adventofcode.com/2022) | 50      | Rust                |
+| [2023](https://adventofcode.com/2023) | 50      | Rust                |
 
-**Standardized** code reads an input file from `stdin` and prints
-```
-Part 1: X
-Part 2: Y
-```
-to `stdout`
-
-2023 is in progress;
-2017-2022 were solved live;
+2017-2023 were solved live;
 2015 and 2016 were solved after 2019.
