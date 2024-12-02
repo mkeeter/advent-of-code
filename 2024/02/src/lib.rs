5722bbc30e18bb59c8f0679cf268404de5a74f85
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-02 07:30:48 -0500

    2024, day 2

--- 2024/02/src/lib.rs
+++ 2024/02/src/lib.rs
@@ -1,3 +1,48 @@
-pub fn solve(_s: &str) -> (String, String) {
-    unimplemented!()
+fn is_safe(row: &[i8]) -> bool {
+    let sign = (row[1] - row[0]).signum();
+    if sign == 0 {
+        return false;
+    }
+    for i in 1..row.len() {
+        let d = row[i] - row[i - 1];
+        if d.signum() != sign || d.abs() > 3 {
+            return false;
+        }
+    }
+    true
+}
+
+fn any_safe(row: &[i8], scratch: &mut Vec<i8>) -> bool {
+    for i in 0..row.len() {
+        scratch.clear();
+        for (j, v) in row.iter().enumerate() {
+            if j != i {
+                scratch.push(*v)
+            }
+        }
+        if is_safe(scratch) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn solve(s: &str) -> (usize, usize) {
+    let mut rows = vec![];
+    for line in s.lines() {
+        rows.push(
+            line.split_ascii_whitespace()
+                .map(|i| i.parse::<i8>().unwrap())
+                .collect::<Vec<_>>(),
+        );
+    }
+
+    let safe = rows.iter().filter(|row| is_safe(row)).count();
+    let mut scratch = vec![];
+    let modified = rows
+        .iter()
+        .filter(|row| any_safe(row, &mut scratch))
+        .count();
+
+    (safe, modified)
 }
