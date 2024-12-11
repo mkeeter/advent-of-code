2710bafc4bae03e680ce63be9e82444b46b80086
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-11 08:09:30 -0500

    Add a helper struct

--- 2024/11/src/lib.rs
+++ 2024/11/src/lib.rs
@@ -1,13 +1,30 @@
 use std::collections::HashMap;
 use util::get_integers;
 
-const MAX_STEP_COUNT: usize = 75;
+struct StepCache<const N: usize>([HashMap<u64, u64>; N]);
+impl<const N: usize> StepCache<N> {
+    fn new() -> Self {
+        Self(std::array::from_fn(|_| HashMap::new()))
+    }
+    fn get(&self, i: u64, steps: usize) -> Option<&u64> {
+        steps
+            .checked_sub(1)
+            .and_then(|s| self.0.get(s))
+            .and_then(|v| v.get(&i))
+    }
+    fn insert(&mut self, i: u64, steps: usize, v: u64) {
+        steps
+            .checked_sub(1)
+            .and_then(|s| self.0.get_mut(s))
+            .and_then(|h| h.insert(i, v));
+    }
+}
 
-pub fn recurse(
+fn recurse<const N: usize>(
     i: u64,
     steps: usize,
-    cache: &mut [HashMap<u64, usize>; MAX_STEP_COUNT],
-) -> usize {
+    cache: &mut StepCache<N>,
+) -> u64 {
     if steps == 0 {
         1
     } else if i == 0 {
@@ -15,13 +32,13 @@ pub fn recurse(
     } else {
         let d = i.ilog10() + 1;
         if d % 2 == 0 {
-            if let Some(v) = cache[steps - 1].get(&i) {
+            if let Some(v) = cache.get(i, steps) {
                 return *v;
             }
             let scale = 10u64.pow(d / 2);
             let v = recurse(i % scale, steps - 1, cache)
                 + recurse(i / scale, steps - 1, cache);
-            cache[steps - 1].insert(i, v);
+            cache.insert(i, steps, v);
             v
         } else {
             recurse(i * 2024, steps - 1, cache)
@@ -29,8 +46,8 @@ pub fn recurse(
     }
 }
 
-pub fn solve(s: &str) -> (usize, usize) {
-    let mut cache = std::array::from_fn(|_| HashMap::new());
+pub fn solve(s: &str) -> (u64, u64) {
+    let mut cache = StepCache::<75>::new();
     get_integers(s)
         .map(|i| (recurse(i, 25, &mut cache), recurse(i, 75, &mut cache)))
         .fold((0, 0), |(a, b), (c, d)| (a + c, b + d))
