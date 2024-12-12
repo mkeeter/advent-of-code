9975e2e46c89d4ee5d7833cc909674c18acfd3ad
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-12 07:49:05 -0500

    Day 12, part 2

--- 2024/12/src/lib.rs
+++ 2024/12/src/lib.rs
@@ -1,21 +1,22 @@
 use util::{Grid, GridSet};
 
-fn check_region(x: i64, y: i64, g: &Grid, seen: &mut GridSet) -> u64 {
+fn check_region(x: i64, y: i64, g: &Grid, seen: &mut GridSet) -> (u64, u64) {
     if seen.contains(x, y) {
-        return 0;
+        return (0, 0);
     }
 
     let c = g[(x, y)];
     let mut todo = vec![(x, y)];
     let mut edge_count = 0;
     let mut cell_count = 0;
+    let mut side_count = 0;
     while let Some((x, y)) = todo.pop() {
         assert_eq!(g[(x, y)], c);
         if !seen.insert(x, y) {
             continue;
         }
         cell_count += 1;
-        for (dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+        for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
             let (nx, ny) = (x + dx, y + dy);
             if g[(nx, ny)] == c {
                 if !seen.contains(nx, ny) {
@@ -25,8 +26,16 @@ fn check_region(x: i64, y: i64, g: &Grid, seen: &mut GridSet) -> u64 {
                 edge_count += 1;
             }
         }
+        let mut corners = 0;
+        for (dx, dy) in [(-1, -1), (-1, 1), (1, 1), (1, -1)] {
+            let convex_corner = g[(x + dx, y)] != c && g[(x, y + dy)] != c;
+            let concave_corner = g[(x + dx, y + dy)] != c
+                && (g[(x + dx, y)] == c && g[(x, y + dy)] == c);
+            corners += (convex_corner || concave_corner) as u64;
+        }
+        side_count += corners;
     }
-    edge_count * cell_count
+    (edge_count * cell_count, side_count * cell_count)
 }
 
 pub fn solve(s: &str) -> (u64, u64) {
@@ -34,12 +43,15 @@ pub fn solve(s: &str) -> (u64, u64) {
     let mut seen = GridSet::new(&g);
 
     let mut score = 0;
+    let mut discount = 0;
     for y in 0..g.height() {
         for x in 0..g.width() {
-            score += check_region(x, y, &g, &mut seen);
+            let (ds, dd) = check_region(x, y, &g, &mut seen);
+            score += ds;
+            discount += dd;
         }
     }
-    (score, 0)
+    (score, discount)
 }
 
 #[cfg(test)]
@@ -60,6 +72,33 @@ mod test {
             MIIISIJEEE
             MMMISSJEEE
         "};
-        assert_eq!(solve(EXAMPLE), (1930, 0));
+        assert_eq!(solve(EXAMPLE), (1930, 1206));
+
+        const SMALL: &str = indoc::indoc! {"
+            AAAA
+            BBCD
+            BBCC
+            EEEC
+        "};
+        assert_eq!(solve(SMALL), (140, 80));
+
+        const MEDIUM: &str = indoc::indoc! {"
+            EEEEE
+            EXXXX
+            EEEEE
+            EXXXX
+            EEEEE
+        "};
+        assert_eq!(solve(MEDIUM).1, 236);
+
+        const LARGE: &str = indoc::indoc! {"
+            AAAAAA
+            AAABBA
+            AAABBA
+            ABBAAA
+            ABBAAA
+            AAAAAA
+        "};
+        assert_eq!(solve(LARGE).1, 368);
     }
 }
