677a7ef060eb263895a59f3df72e0db2b3c9b276
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-15 10:47:45 -0500

    Use an enum instead of a char -> 40% speedup

--- 2024/15/src/lib.rs
+++ 2024/15/src/lib.rs
@@ -1,5 +1,17 @@
 use std::collections::HashMap;
 
+enum Cell {
+    Box,
+    Wall,
+}
+
+#[derive(Clone)]
+enum WideCell {
+    LeftBox,
+    RightBox,
+    Wall,
+}
+
 pub fn solve(s: &str) -> (u64, u64) {
     let mut grid = HashMap::new();
     let mut start = None;
@@ -10,12 +22,17 @@ pub fn solve(s: &str) -> (u64, u64) {
             map = false;
         }
         if map {
+            let y = y as i64;
             for (x, c) in line.chars().enumerate() {
+                let x = x as i64;
                 match c {
                     '.' => (),
-                    '@' => start = Some((x as i64, y as i64)),
-                    '#' | 'O' => {
-                        grid.insert((x as i64, y as i64), c);
+                    '@' => start = Some((x, y)),
+                    '#' => {
+                        grid.insert((x, y), Cell::Wall);
+                    }
+                    'O' => {
+                        grid.insert((x, y), Cell::Box);
                     }
                     _ => panic!("invalid grid character '{c}'"),
                 }
@@ -39,36 +56,32 @@ pub fn solve(s: &str) -> (u64, u64) {
         let ny = y + dy;
         match grid.get(&(nx, ny)) {
             None => (),
-            Some('#') => continue, // bonk
-            Some('O') => {
+            Some(Cell::Wall) => continue, // bonk
+            Some(Cell::Box) => {
                 let (mut gx, mut gy) = (nx, ny);
                 let (gx, gy) = loop {
                     gx += dx;
                     gy += dy;
                     match grid.get(&(gx, gy)) {
                         None => break (gx, gy),
-                        Some('#') => continue 'outer,
-                        Some('O') => (),
-                        c => panic!("invalid grid value {c:?}"),
+                        Some(Cell::Wall) => continue 'outer,
+                        Some(Cell::Box) => (),
                     }
                 };
                 let v = grid.remove(&(nx, ny)).unwrap();
-                assert_eq!(v, 'O');
+                assert!(matches!(v, Cell::Box));
                 let prev = grid.insert((gx, gy), v);
                 assert!(prev.is_none());
             }
-            c => panic!("invalid grid value {c:?}"),
         }
         (x, y) = (nx, ny);
     }
 
-    let score = |grid: &HashMap<(i64, i64), char>, c| {
-        grid.iter()
-            .filter(|(_p, v)| **v == c)
-            .map(|((x, y), _v)| (y * 100 + x) as u64)
-            .sum()
-    };
-    let part1 = score(&grid, 'O');
+    let part1 = grid
+        .iter()
+        .filter(|(_p, v)| matches!(*v, Cell::Box))
+        .map(|((x, y), _v)| (y * 100 + x) as u64)
+        .sum();
 
     // reparse into a wiiiiiiiiiiide grid
     let mut grid = HashMap::new();
@@ -84,12 +97,12 @@ pub fn solve(s: &str) -> (u64, u64) {
                 '.' => (),
                 '@' => start = Some((x * 2, y)),
                 '#' => {
-                    grid.insert((x * 2, y), '#');
-                    grid.insert((x * 2 + 1, y), '#');
+                    grid.insert((x * 2, y), WideCell::Wall);
+                    grid.insert((x * 2 + 1, y), WideCell::Wall);
                 }
                 'O' => {
-                    grid.insert((x * 2, y), '[');
-                    grid.insert((x * 2 + 1, y), ']');
+                    grid.insert((x * 2, y), WideCell::LeftBox);
+                    grid.insert((x * 2 + 1, y), WideCell::RightBox);
                 }
                 _ => panic!("invalid grid character '{c}'"),
             }
@@ -98,37 +111,39 @@ pub fn solve(s: &str) -> (u64, u64) {
     let (mut x, mut y) = start.expect("could not find start");
     for &(dx, dy) in &commands {
         assert!(!grid.contains_key(&(x, y)));
-
         let nx = x + dx;
         let ny = y + dy;
         match grid.get(&(nx, ny)) {
             None => (),
-            Some('#') => continue, // bonk
-            Some('[' | ']') if dy == 0 => {
+            Some(WideCell::Wall) => continue, // bonk
+            Some(WideCell::LeftBox | WideCell::RightBox) if dy == 0 => {
                 if !push_hbox((nx, ny), dx, &mut grid) {
                     continue;
                 }
             }
-            Some('[') => {
+            Some(WideCell::LeftBox) => {
                 let mut temp = grid.clone();
                 if !push_vbox((nx, ny), dy, &mut temp) {
                     continue;
                 }
                 grid = temp;
             }
-            Some(']') => {
+            Some(WideCell::RightBox) => {
                 let mut temp = grid.clone();
                 if !push_vbox((nx - 1, ny), dy, &mut temp) {
                     continue;
                 }
                 grid = temp;
             }
-            c => panic!("invalid grid value {c:?}"),
         }
         assert!(!grid.contains_key(&(nx, ny)));
         (x, y) = (nx, ny);
     }
-    let part2 = score(&grid, '[');
+    let part2 = grid
+        .iter()
+        .filter(|(_p, v)| matches!(**v, WideCell::LeftBox))
+        .map(|((x, y), _v)| (y * 100 + x) as u64)
+        .sum();
 
     (part1, part2)
 }
@@ -136,7 +151,7 @@ pub fn solve(s: &str) -> (u64, u64) {
 fn push_hbox(
     corner: (i64, i64),
     dx: i64,
-    grid: &mut HashMap<(i64, i64), char>,
+    grid: &mut HashMap<(i64, i64), WideCell>,
 ) -> bool {
     // Horizontal pushing is easy!
     let (mut gx, gy) = corner;
@@ -146,15 +161,14 @@ fn push_hbox(
             None => {
                 break;
             }
-            Some('#') => return false, // bonk
-            Some('[' | ']') => (),
-            Some(c) => panic!("invalid grid value {c:?}"),
+            Some(WideCell::Wall) => return false, // bonk
+            Some(WideCell::LeftBox | WideCell::RightBox) => (),
         }
     }
     // Slide all of the boxes along
     while gx != corner.0 {
         let v = grid.remove(&(gx - dx, gy)).unwrap();
-        assert!(v == '[' || v == ']', "bad hbox '{v}'");
+        assert!(matches!(v, WideCell::LeftBox | WideCell::RightBox));
         let prev = grid.insert((gx, gy), v);
         assert!(prev.is_none());
         gx -= dx;
@@ -165,89 +179,42 @@ fn push_hbox(
 fn push_vbox(
     corner: (i64, i64),
     dy: i64,
-    grid: &mut HashMap<(i64, i64), char>,
+    grid: &mut HashMap<(i64, i64), WideCell>,
 ) -> bool {
-    assert_eq!(grid[&corner], '[');
-    assert_eq!(grid[&(corner.0 + 1, corner.1)], ']');
+    assert!(matches!(grid[&corner], WideCell::LeftBox));
+    assert!(matches!(
+        grid[&(corner.0 + 1, corner.1)],
+        WideCell::RightBox
+    ));
 
     for offset in [0, 1] {
         let (nx, ny) = (corner.0 + offset, corner.1 + dy);
         match grid.get(&(nx, ny)) {
             None => (),
-            Some('#') => return false, // bonk
-            Some('[') => {
+            Some(WideCell::Wall) => return false, // bonk
+            Some(WideCell::LeftBox) => {
                 if !push_vbox((nx, ny), dy, grid) {
                     return false;
                 }
             }
-            Some(']') => {
+            Some(WideCell::RightBox) => {
                 if !push_vbox((nx - 1, ny), dy, grid) {
                     return false;
                 }
             }
-            Some(c) => panic!("invalid grid value '{c}'"),
         }
     }
     let v = grid.remove(&corner).unwrap();
-    assert_eq!(v, '[');
+    assert!(matches!(v, WideCell::LeftBox));
     let prev = grid.insert((corner.0, corner.1 + dy), v);
     assert!(prev.is_none());
 
     let v = grid.remove(&(corner.0 + 1, corner.1)).unwrap();
-    assert_eq!(v, ']');
+    assert!(matches!(v, WideCell::RightBox));
     let prev = grid.insert((corner.0 + 1, corner.1 + dy), v);
     assert!(prev.is_none());
 
     true
-}
-
-#[allow(unused)]
-fn print_grid(pos: (i64, i64), grid: &HashMap<(i64, i64), char>) {
-    let width = grid.keys().map(|(x, _)| x).max().cloned().unwrap_or(0);
-    let height = grid.keys().map(|(_, y)| y).max().cloned().unwrap_or(0);
-    for y in 0..=height {
-        for x in 0..=width {
-            let p = (x, y);
-            if p == pos {
-                assert!(!grid.contains_key(&p));
-                print!("@");
-            } else {
-                print!("{}", grid.get(&p).unwrap_or(&'.'));
-            }
-        }
-        println!();
-    }
-}
-
-#[allow(unused)]
-fn check_grid(
-    pos: (i64, i64),
-    grid: &HashMap<(i64, i64), char>,
-) -> (usize, usize) {
-    let width = grid.keys().map(|(x, _)| x).max().cloned().unwrap_or(0);
-    let height = grid.keys().map(|(_, y)| y).max().cloned().unwrap_or(0);
-    let mut box_count = 0;
-    let mut wall_count = 0;
-    for y in 0..=height {
-        for x in 0..=width {
-            let p = (x, y);
-            if p == pos {
-                assert!(!grid.contains_key(&p));
-            } else {
-                match grid.get(&p) {
-                    None => (),
-                    Some('#') => wall_count += 1,
-                    Some('[') => {
-                        box_count += 1;
-                        assert_eq!(grid.get(&(x + 1, y)), Some(&']'))
-                    }
-                    Some(']') => assert_eq!(grid.get(&(x - 1, y)), Some(&'[')),
-                    Some(c) => panic!("invalid grid cell '{c}'"),
-                }
-            }
-        }
-    }
-    (box_count, wall_count)
 }
 
 #[cfg(test)]
