636f7835841e040c52609e5405201ca32e37cade
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-16 08:55:53 -0500

    Use TupleSet elsewhere

--- 2024/06/src/lib.rs
+++ 2024/06/src/lib.rs
@@ -1,5 +1,5 @@
 use rayon::prelude::*;
-use util::{BitSet, Grid};
+use util::{Grid, TupleSet};
 
 pub fn solve(s: &str) -> (usize, usize) {
     let g = Grid::new(s);
@@ -16,14 +16,12 @@ pub fn solve(s: &str) -> (usize, usize) {
         panic!("could not find guard's starting position");
     };
     assert_eq!(g.width(), g.height());
-    let stride = (g.width() as usize).next_power_of_two() as i64;
-
-    let mut seen = BitSet::new(stride.pow(2) as usize);
+    let mut seen = TupleSet::new((g.width(), g.height()));
     {
         let (mut x, mut y) = start;
         let (mut dx, mut dy) = (0i64, -1i64);
         loop {
-            seen.set((x + y * stride) as usize);
+            seen.insert((x, y));
             match g.get(x + dx, y + dy) {
                 Some(&b'#') => {
                     (dx, dy) = (-dy, dx);
@@ -42,13 +40,13 @@ pub fn solve(s: &str) -> (usize, usize) {
         .into_par_iter()
         .flat_map(|by| (0..g.width()).into_par_iter().map(move |bx| (bx, by)))
         .filter(|&(bx, by)| {
-            if !seen.get((bx + by * stride) as usize) {
+            if !seen.contains((bx, by)) {
                 return false;
             }
             let (mut x, mut y) = start;
             let (mut dx, mut dy) = (0i64, -1i64);
             let mut angle = 0;
-            let mut seen = BitSet::new((stride.pow(2) * 4) as usize);
+            let mut seen = TupleSet::new((g.width(), g.height(), 4));
             loop {
                 let rot = (x + dx == bx && y + dy == by)
                     || match g.get(x + dx, y + dy) {
@@ -57,8 +55,7 @@ pub fn solve(s: &str) -> (usize, usize) {
                         None => return false,
                     };
                 if rot {
-                    let i = (x + (y + angle * stride) * stride) as usize;
-                    if !seen.insert(i) {
+                    if !seen.insert((x, y, angle)) {
                         break true;
                     }
                     (dx, dy) = (-dy, dx);
