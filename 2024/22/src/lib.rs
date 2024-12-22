932ec0a8e0ddbb6b48f9b143ddb861189f2ccf77
Matt Keeter <matt.j.keeter@gmail.com>
2024-12-22 08:33:23 -0500

    rayon time

--- 2024/22/src/lib.rs
+++ 2024/22/src/lib.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use rayon::prelude::*;
+use std::collections::{HashMap, HashSet};
 use util::get_integers;
 
 const MASK: u32 = 0xFFFFFF;
@@ -9,7 +10,7 @@ fn step(mut v: u32) -> u32 {
     ((v << 11) ^ v) & MASK
 }
 
-fn run(seed: u32, cache: &mut HashMap<u32, HashMap<u32, u8>>) -> u32 {
+fn run(seed: u32, cache: &mut HashMap<u32, (u64, HashSet<u32>)>) -> u32 {
     let mut v = seed;
     let mut key = 0u32; // [4 x i8]
     let mut prev_price = (v % 10) as i8;
@@ -19,11 +20,10 @@ fn run(seed: u32, cache: &mut HashMap<u32, HashMap<u32, u8>>) -> u32 {
         let delta = price - prev_price;
         key = (key >> 8) | (delta as u8 as u32) << 24;
         if r >= 3 {
-            cache
-                .entry(key)
-                .or_default()
-                .entry(seed)
-                .or_insert(price as u8);
+            let e = cache.entry(key).or_default();
+            if e.1.insert(seed) {
+                e.0 += price as u8 as u64;
+            }
         }
         prev_price = price;
     }
@@ -31,18 +31,29 @@ fn run(seed: u32, cache: &mut HashMap<u32, HashMap<u32, u8>>) -> u32 {
 }
 
 pub fn solve(s: &str) -> (u64, u64) {
-    let mut sum = 0;
     let seeds = get_integers::<u32>(s).collect::<Vec<_>>();
-    let mut prices = HashMap::new();
-    for i in seeds.iter().cloned() {
-        sum += u64::from(run(i, &mut prices));
-    }
+    let (prices, sum) = seeds
+        .par_iter()
+        .fold(
+            || (HashMap::new(), 0),
+            |(mut prices, sum), i| {
+                let v = u64::from(run(*i, &mut prices));
+                (prices, sum + v)
+            },
+        )
+        .reduce(
+            || (HashMap::new(), 0),
+            |(mut prices, sum), (p, s)| {
+                for (k, v) in p {
+                    // We just care about merging the sums; we can ignore the
+                    // secondary hashset of which values have been seen
+                    prices.entry(k).or_default().0 += v.0;
+                }
+                (prices, sum + s)
+            },
+        );
 
-    let best_price = prices
-        .values()
-        .map(|k| k.values().map(|v| u64::from(*v)).sum())
-        .max()
-        .unwrap();
+    let best_price = prices.values().map(|k| k.0).max().unwrap();
 
     (sum, best_price)
 }
@@ -61,7 +72,6 @@ mod test {
         "};
         assert_eq!(solve(EXAMPLE_1).0, 37327623);
 
-        println!("example 2");
         const EXAMPLE_2: &str = indoc::indoc! {"
             1
             2
