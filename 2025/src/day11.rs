24a12830ad0ea13d793338687db29097eb78b9d4
Matt Keeter <matt.j.keeter@gmail.com>
2025-12-11 08:01:54 -0500

    Day 11, part 2

--- 2025/src/day11.rs
+++ 2025/src/day11.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-pub fn solve(s: &str) -> (u64, u64) {
+fn get_edges(s: &str) -> HashMap<&str, Vec<&str>> {
     let mut edges = HashMap::new();
     for line in s.lines() {
         let mut iter = line.split(": ");
@@ -11,27 +11,57 @@ pub fn solve(s: &str) -> (u64, u64) {
         let prev = edges.insert(start, ends);
         assert!(prev.is_none(), "duplicate start node '{start}'");
     }
-    let part1 = search("you", "out", &edges, &mut Default::default());
-    (part1, 0)
+    edges
+}
+
+fn part1<'a, 'b>(edges: &'b HashMap<&'a str, Vec<&'a str>>) -> u64 {
+    search(
+        "you",
+        "out",
+        &edges,
+        &mut Default::default(),
+        &mut Default::default(),
+    )
+}
+
+fn part2<'a, 'b>(edges: &'b HashMap<&'a str, Vec<&'a str>>) -> u64 {
+    let mut required = ["dac", "fft"].into_iter().collect();
+    search("svr", "out", &edges, &mut required, &mut Default::default())
+}
+
+pub fn solve(s: &str) -> (u64, u64) {
+    let edges = get_edges(s);
+    (part1(&edges), part2(&edges))
 }
 
 fn search<'a, 'b>(
     start: &'a str,
     end: &'a str,
     edges: &'b HashMap<&'a str, Vec<&'a str>>,
-    cache: &'b mut HashMap<(&'a str, &'a str), u64>,
+    required: &'b mut HashSet<&'a str>,
+    cache: &'b mut HashMap<Vec<&'a str>, u64>,
 ) -> u64 {
     if start == end {
-        1
-    } else if let Some(v) = cache.get(&(start, end)) {
-        *v
+        if required.is_empty() { 1 } else { 0 }
     } else {
-        let out = edges[start]
-            .iter()
-            .map(|start| search(start, end, edges, cache))
-            .sum();
-        cache.insert((start, end), out);
-        out
+        let mut key = required.iter().cloned().collect::<Vec<_>>();
+        key.sort();
+        key.push(start);
+        key.push(end);
+        if let Some(v) = cache.get(&key) {
+            *v
+        } else {
+            let prev = required.remove(start);
+            let out = edges[start]
+                .iter()
+                .map(|start| search(start, end, edges, required, cache))
+                .sum();
+            cache.insert(key, out);
+            if prev {
+                required.insert(start);
+            }
+            out
+        }
     }
 }
 
@@ -40,7 +70,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_part1() {
         let s = indoc::indoc! {"
             aaa: you hhh
             you: bbb ccc
@@ -53,8 +83,28 @@ mod test {
             hhh: ccc fff iii
             iii: out
         "};
-        let (a, b) = solve(s);
-        assert_eq!(a, 5);
-        assert_eq!(b, 0);
+        let edges = get_edges(s);
+        assert_eq!(part1(&edges), 5);
+    }
+
+    #[test]
+    fn test_part2() {
+        let s = indoc::indoc! {"
+            svr: aaa bbb
+            aaa: fft
+            fft: ccc
+            bbb: tty
+            tty: ccc
+            ccc: ddd eee
+            ddd: hub
+            hub: fff
+            eee: dac
+            dac: fff
+            fff: ggg hhh
+            ggg: out
+            hhh: out
+        "};
+        let edges = get_edges(s);
+        assert_eq!(part2(&edges), 2);
     }
 }
